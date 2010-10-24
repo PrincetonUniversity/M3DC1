173f5aa4283f5ef056dbfbe949c79aae9305f470
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2010-10-24 18:02:34 +0000

    * Corrected an error in multipole field definition * Changed 3D diffusion test * Improvements to 3D IDL capabilities

--- unstructured/idl/read_h5.pro
+++ unstructured/idl/read_h5.pro
@@ -5352,15 +5352,15 @@ pro plot_field_3d, fieldname, contrast=contrast, flux=flux, $
       endif else if(n_elements(zslice) eq 1) then begin
           value = zslice
           normal = [0,0,1]
-      endif
-      if(n_elements(value) ne 0) then begin
-          plot_slice, data, x, phi, z, value=value, normal=normal
-          surface, tmpdat, xrange, yrange, xstyle=1, ystyle=1, zstyle=1, $
-            /nodata, /noerase, ax=ax, az=az, charsize=2.5, $
-            xrange=xrange, yrange=yrange, zrange=zrange, $
-            xtitle='R', ytitle='!9P!X', ztitle='Z'
-          return
-      endif
+      endif else begin
+          value = 0.9
+      endelse
+      plot_slice, data, x, phi, z, value=value, normal=normal
+      surface, tmpdat, xrange, yrange, xstyle=1, ystyle=1, zstyle=1, $
+        /nodata, /noerase, ax=ax, az=az, charsize=2.5, $
+        xrange=xrange, yrange=yrange, zrange=zrange, $
+        xtitle='R', ytitle='!9P!X', ztitle='Z'
+      return
   endelse
    
   ; reduce values at edge
@@ -5419,4 +5419,28 @@ pro plot_field_3d, fieldname, contrast=contrast, flux=flux, $
     xrange=xrange, yrange=yrange, zrange=zrange, $
     xtitle='R', ytitle='!9P!X', ztitle='Z'
 
+end
+
+pro movie_3d, fieldname, mpeg=mpeg, _EXTRA=extra
+   if(n_elements(mpeg) eq 0) then mpeg = fieldname + '.mpeg'
+   ntime = read_parameter('ntime', _EXTRA=extra)
+   print, 'ntime = ', ntime
+
+   mpegid = mpeg_open([640,480],bitrate=104857200, iframe_gap=4)
+
+   for i=0, ntime-1 do begin
+       plot_field_3d, fieldname, slice=i, _EXTRA=extra
+       image = tvrd(true=1)
+               
+       image[0,*,*] = rotate(reform(image[0,*,*]), 7)
+       image[1,*,*] = rotate(reform(image[1,*,*]), 7)
+       image[2,*,*] = rotate(reform(image[2,*,*]), 7)
+
+       mpeg_put, mpegid, image=image, frame=5*i
+   end
+
+   print, 'Writing ', mpeg, '...'
+   mpeg_save, mpegid, filename=mpeg
+   mpeg_close, mpegid
+   print, 'Done.'
 end
--- unstructured/init_conds.f90
+++ unstructured/init_conds.f90
@@ -2374,6 +2374,7 @@ subroutine threed_wave_test_init()
   kphi = ntor/rzero
 
   omega = (rzero*bzero)*kphi
+  if(myrank.eq.0) print *, 'kphi = ', kphi
   if(myrank.eq.0) print *, 'omega = ', omega
 
   numnodes = owned_nodes()
@@ -2448,7 +2449,7 @@ end module threed_wave_test
 !==============================================================================
 module threed_diffusion_test
 
-  real, private :: kx, kz, kphi, phi0
+  real, private :: kx, kz
 
 contains
 
@@ -2462,29 +2463,28 @@ subroutine threed_diffusion_test_init()
 
   integer :: l, numnodes
   real :: x, phi, z, x1, x2, z1, z2
+  real :: phi0, x0, z0
 
   call get_bounding_box(x1, z1, x2, z2)
   kx = pi/(x2-x1)
   kz = pi/(z2-z1)
-  kphi = ntor/rzero
 
 #ifdef USE3D
   phi0 = pi
 #else
   phi0 = 0
 #endif
+  z0 = (z1 + z2)/2.
+  x0 = (x1 + 2.*x2)/3.
 
   numnodes = owned_nodes()
   do l=1, numnodes
      call get_node_pos(l, x, phi, z)
 
-     x = x - x1
-     z = z - z1
-
      call get_local_vals(l)
 
-     call threed_diffusion_test_equ(x, phi, z)
-     call threed_diffusion_test_per(x, phi, z)
+     call threed_diffusion_test_equ(x-x1, phi, z)
+     call threed_diffusion_test_per(x-x0, phi-phi0, z-z0)
 
      call set_local_vals(l)
   enddo
@@ -2498,7 +2498,12 @@ subroutine threed_diffusion_test_equ(x, phi, z)
 
   real, intent(in) :: x, phi, z
 
-  psi0_l = 0.
+  psi0_l(1) = bx0*sin(kx*x)*sin(kz*z)
+  psi0_l(2) = bx0*cos(kx*x)*sin(kz*z)
+  psi0_l(3) = bx0*sin(kx*x)*cos(kz*z)
+  psi0_l(4) =-bx0*sin(kx*x)*sin(kz*z)
+  psi0_l(5) = bx0*cos(kx*x)*cos(kz*z)
+  psi0_l(6) =-bx0*sin(kx*x)*sin(kz*z)
 
   call constant_field(bz0_l, bzero)
   call constant_field(den0_l, 1.)
@@ -2515,27 +2520,21 @@ subroutine threed_diffusion_test_per(x, phi, z)
   implicit none
 
   real, intent(in) :: x, phi, z
-  real :: ex, dex
+  real :: ex
 
-  ex = eps*exp(-(kphi*(phi-phi0))**2)
-  dex = 2.*kphi**2*(phi-phi0)*ex
+  ex = eps*exp(-(rzero*phi/ln)**2-(x/ln)**2-(z/ln)**2)
 
-  p1_l(1) = sin(kx*x)*sin(kz*z)*ex
-  p1_l(2) = cos(kx*x)*sin(kz*z)*ex*kx
-  p1_l(3) = sin(kx*x)*cos(kz*z)*ex*kz
-  p1_l(4) =-sin(kx*x)*sin(kz*z)*ex*kx**2
-  p1_l(5) = cos(kx*x)*cos(kz*z)*ex*kx*kz
-  p1_l(6) =-sin(kx*x)*sin(kz*z)*ex*kz**2
+  p1_l(1) = ex
+  p1_l(2) = -2.*ex*x/ln**2
+  p1_l(3) = -2.*ex*z/ln**2
+  p1_l(4) = 4.*ex*x**2/ln**4 - 2.*ex*ln**2
+  p1_l(5) = 4.*ex*x*z/ln**4
+  p1_l(6) = 4.*ex*z**2/ln**4 - 2.*ex*ln**2
 #ifdef USE3D
-  p1_l(7)  =-sin(kx*x)*sin(kz*z)*dex
-  p1_l(8)  =-cos(kx*x)*sin(kz*z)*dex*kx
-  p1_l(9)  =-sin(kx*x)*cos(kz*z)*dex*kz
-  p1_l(10) = sin(kx*x)*sin(kz*z)*dex*kx**2
-  p1_l(11) =-cos(kx*x)*cos(kz*z)*dex*kx*kz
-  p1_l(12) = sin(kx*x)*sin(kz*z)*dex*kz**2
+  p1_l(7:12) = -2.*phi*p1_l(1:6)*(rzero/ln)**2
 #endif
 
-  den1_l = p1_l
+  if(idens.eq.1) den1_l = p1_l
   pe1_l = p1_l*pefac
 
 end subroutine threed_diffusion_test_per
