2ddf366795de7c0f1d276d10069db6dbf2064d88
sjardin <sjardin@pppl.gov>
2011-01-30 21:51:15 +0000

    Made some changes in the RWM analytic current decay problem itaylor=12.

--- unstructured/ic_resistive_wall.f90
+++ unstructured/ic_resistive_wall.f90
@@ -2,7 +2,7 @@
 ! Resistive wall test
 !==============================================================================
 module resistive_wall_test
-  real, parameter, private :: a = 1.
+  real, parameter, private :: a = .1
   real, private :: k, Im_ka
 
   private :: analytic_response_matrix
@@ -18,7 +18,7 @@ contains
     real :: ka, bessk, besskp
     real, allocatable :: theta(:)
     integer :: ierr, i, j, m
-    complex :: fac, fac1, fac2
+    complex :: fac, fac1, fac2, expimth
 
     print *, 'Using analytic response matrix.'
 
@@ -37,28 +37,30 @@ contains
     zgrbph = 0.
     zgrbphp = 0.
     m = mpol
-    do m=-30, 30
-       fac = (1./(nodes*a))*bessk(m,ka)/besskp(m,ka)
-       fac1 = -m/ka * fac
-       fac2 = (0.,1.) * fac
+    do m=mpol,mpol
+       fac =  (1./(nodes))*bessk(m,ka)/besskp(m,ka)
+       fac1 =  (0.,1.)*m/ka * fac
+       fac2 = +(0.,1.) * fac
+!      fac2 = -(0.,1.) * fac
        do i=1,nodes+1
           do j=1,nodes+1
-             zgrbth(i,j) = zgrbth(i,j) + fac1*sin(m*(theta(i) - theta(j)))
-             zgrbph(i,j) = zgrbph(i,j) + fac2*cos(m*(theta(i) - theta(j)))
+             expimth = cos(m*(theta(i) - theta(j))) + (0.,1.)*sin(m*(theta(i) - theta(j)))
+             zgrbth(i,j) = zgrbth(i,j) + fac1*expimth
+             zgrbph(i,j) = zgrbph(i,j) + fac2*expimth
           enddo
        enddo
     end do
 
     ! calculate derivatives (wrt i)
     zgrbthp(1,:) = (zgrbth(2,:) - zgrbth(nodes+1,:)) &
-         /(theta(2) - theta(nodes+1))
+         /(theta(2) - theta(nodes+1))/a
     zgrbphp(1,:) = (zgrbph(2,:) - zgrbph(nodes+1,:)) &
-         /(theta(2) - theta(nodes+1))
+         /(theta(2) - theta(nodes+1))/a
     do i=2,nodes
        zgrbthp(i,:) = (zgrbth(i+1,:) - zgrbth(i-1,:)) &
-            /(theta(i+1) - theta(i-1))
+            /(theta(i+1) - theta(i-1))/a 
        zgrbphp(i,:) = (zgrbph(i+1,:) - zgrbph(i-1,:)) &
-            /(theta(i+1) - theta(i-1))
+            /(theta(i+1) - theta(i-1))/a
     end do
 
     deallocate(theta)
@@ -134,8 +136,10 @@ subroutine resistive_wall_test_equ(x, z)
   psi0_l(5) = 0.
   psi0_l(6) = 2.
     
-  call constant_field(bz0_l , bzero*rzero)
   call constant_field(den0_l, 1.)
+  if(numvar.le.1) return
+
+  call constant_field(bz0_l , bzero*rzero)
   call constant_field(p0_l , p0)
   call constant_field(pe0_l, p0-pi0*idens)
 
@@ -148,20 +152,20 @@ subroutine resistive_wall_test_per(x, z)
   implicit none
 
   real, intent(in) :: x, z
-  real :: theta, r, co, sn, ram, Im_kr, Imp_kr, Impp_kr
+  real :: theta, r, ram, Im_kr, Imp_kr, Impp_kr
   real :: bessi
-  complex :: fac
+  complex :: fac, co, sn
 
 
   r = sqrt(x**2 + z**2)
 
   if(r.eq.0.) then
      psi1_l = 0.
-     bz1_l = 0.
+     if(numvar.gt.1) bz1_l = 0.
   else
      theta = atan2(z, x)
-     co = cos(mpol*theta)
-     sn = sin(mpol*theta)
+     co = cos(mpol*theta) + (0.,1.)*sin(mpol*theta)
+     sn = sin(mpol*theta) - (0.,1.)*cos(mpol*theta)
      ram = (r/a)**mpol
 
      psi1_l(1) = co*ram
@@ -170,7 +174,9 @@ subroutine resistive_wall_test_per(x, z)
      psi1_l(4) = mpol*(mpol-1) * (ram/r**4) * ((x**2 - z**2)*co + 2.*x*z*sn)
      psi1_l(5) = mpol*(mpol-1) * (ram/r**4) * (2.*x*z*co - (x**2 - z**2)*sn)
      psi1_l(6) = -psi1_l(4)
+     psi1_l = psi1_l*eps
      
+  if(numvar.le.1) return
      fac = (0.,1.)*k/Im_ka
      if(itor.eq.1) fac = fac*rzero
      Im_kr = bessi(mpol, k*r)
@@ -187,7 +193,6 @@ subroutine resistive_wall_test_per(x, z)
      bz1_l(6) = -(fac/r**4)*(-mpol*x*(2.*z*co+mpol*x*sn)*Im_kr &
           + k*r*x*(2.*mpol*z*co+x*sn)*Imp_kr + (k*r*z)**2*sn*Impp_kr)
      
-     psi1_l = psi1_l*eps
      bz1_l = bz1_l*eps
   end if
 
@@ -211,33 +216,46 @@ contains
 
     implicit none
 
-    real :: ka, thetai, thetaj, fac, bessk, besskp, grate, a
+    real, allocatable :: theta(:)
+    real :: ka,  bessk, besskp, grate, a
     integer :: ierr, i, j, m
+  complex :: fac
 
     print *, 'Using analytic response matrix.'
 
     call load_boundary_nodes(ierr)
+    allocate(theta(nodes+1))
+    do i=1, nodes+1
+       theta(i) = atan2(znode(i)-zzero,xnode(i)-xzero)
+    end do
     if(ierr.ne.0) return
 
     !...  define matrices with analytic formula
-    a = 1.
+    a = .1
     ka = a*ntor/rzero
 
     zgrbth = 0.
-    do m=1, 30
-       fac = 2.*m/(nodes*ka)*bessk(m,ka)/besskp(m,ka)
+    do m=mpol,mpol
+       fac = (0.,1.)*m/(nodes*ka)*bessk(m,ka)/besskp(m,ka)
        do i=1,nodes+1
           do j=1,nodes+1
-             thetai = atan2(znode(i)-zzero,xnode(i)-xzero)
-             thetaj = atan2(znode(j)-zzero,xnode(j)-xzero)
-             zgrbth(i,j) = zgrbth(i,j) + fac*sin(m*(thetai-thetaj))
+             zgrbth(i,j) = zgrbth(i,j) + fac*(cos(m*(theta(i) - theta(j)))  &
+                                   + (0.,1.)* sin(m*(theta(i) - theta(j))))
           enddo
        enddo
     end do
+    ! calculate derivatives (wrt i)
+    zgrbthp(1,:) = (zgrbth(2,:) - zgrbth(nodes+1,:)) &
+         /(theta(2) - theta(nodes+1))/a
+    do i=2,nodes
+       zgrbthp(i,:) = (zgrbth(i+1,:) - zgrbth(i-1,:)) &
+            /(theta(i+1) - theta(i-1))/a
+    end do
     grate = (eta_wall/delta_wall)*(mpol/a)* &
-         (1. + (mpol/ka)*bessk(mpol,ka)/besskp(mpol,ka))
+         (1. - (mpol/ka)*bessk(mpol,ka)/besskp(mpol,ka))
     write(*,'(A,1pe12.4)') " Analytic decay rate",  grate
     write(97,'(A,1pe12.4)') " Analytic decay rate",  grate
+    deallocate(theta)
 
   end subroutine analytic_response_matrix
 
@@ -316,7 +334,8 @@ contains
     implicit none
     
     real, intent(in) :: x, z
-    real ::  k, r, kr, theta, sinm, cosm
+    real ::  k, r, kr, theta
+    complex :: cosm, sinm
 
     den1_l = 0.
     
@@ -327,8 +346,8 @@ contains
     k = ntor/rzero
     kr = k*r
     theta = atan2(z,x)
-    sinm = sin(mpol*theta)
-    cosm = cos(mpol*theta)
+    cosm = cos(mpol*theta) + (0.,1.)*sin(mpol*theta)
+    sinm = - (0.,1.)*cosm
 
     !..for del_perp_squared
     psi1_l(1) = eps*cosm * r**mpol
