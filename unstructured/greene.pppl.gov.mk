48c6095d54c0486459b13659c9ae96580c1414f8
sjardin <sjardin@pppl.gov>
2012-01-06 17:12:33 +0000

    make file for greene

---
+++ unstructured/greene.pppl.gov.mk
@@ -0,0 +1,119 @@
+#FOPTS = -c -r8 -implicitnone -fpp -warn all -DxPetscDEV -DPETSC_31 $(OPTS)
+FOPTS = -c -r8 -implicitnone -fpp -warn all -DPetscDEV -DPETSC_31 -DxCJ_MATRIX_DUMP $(OPTS)
+CCOPTS  = -c -O -DPetscDEV -DxCJ_MATRIX_DUMP -DxUSEHYBRID #-DPETSC_31 
+
+ifeq ($(OPT), 1)
+  FOPTS  := $(FOPTS) -fast
+  CCOPTS := $(CCOPTS) -O
+else
+  FOPTS := $(FOPTS) -g -check all -check noarg_temp_created -debug all -ftrapuv
+endif
+
+ifeq ($(TAU), 1)
+  TAU_OPTIONS = -optCPPOpts=-DUSETAU -optVerbose -optPreProcess -optMpi -optTauSelectFile=../select.tau
+  CC     = tau_cc.sh $(TAU_OPTIONS)
+  CPP    = tau_cxx.sh $(TAU_OPTIONS)
+  F90    = tau_f90.sh $(TAU_OPTIONS)
+  F77    = tau_f90.sh $(TAU_OPTIONS)
+  LOADER = tau_f90.sh $(TAU_OPTIONS)
+else
+  CC = mpicc
+  CPP = mpicxx
+  F90 = mpif90
+  F77 = mpif90
+  LOADER = mpif90 -cxxlib
+  FOPTS := $(FOPTS)
+endif
+F90OPTS = $(F90FLAGS) $(FOPTS) -gen-interfaces
+F77OPTS = $(F77FLAGS) $(FOPTS)
+
+
+# define where you want to locate the mesh adapt libraries
+HYBRID_HOME = /p/swim/jchen/pdslin_0.0
+#HYBRID_HOME = /p/swim/jchen/hybrid.test
+#HYBRID_HOME = /u/iyamazak/release/v2/hybrid.test
+HYBRID_LIBS = -L$(HYBRID_HOME)/lib -lhsolver
+
+#SUPERLU_HOME = $(PETSC_DIR)/$(PETSC_ARCH)
+#SUPERLU_DIST_HOME = $(PETSC_DIR)/$(PETSC_ARCH)
+SUPERLU_DIST_HOME = /p/tsc/m3dc1/lib/SCORECLib/superlu_dist
+INCLUDE = -I$(MPIHOME)/include \
+	-I$(PETSC_DIR)/include -I$(PETSC_DIR)/$(PETSC_ARCH)/include -I$(SUPERLU_DIST_HOME)/include \
+	-I$(HDF5_HOME)/include -I$(HDF5_HOME)/lib \
+	-I$(HYBRID_HOME)/include
+
+
+PETSC_LIBS = -L$(PETSC_DIR)/lib \
+   -lpetsc \
+
+#	-L$(MUMPS_HOME)/lib -ldmumps -lmumps_common -lpord \
+#	-L$(SCALAPACK_HOME) -lscalapack \
+#	-L$(BLACS_HOME)/lib -lmpiblacsF77init -lmpiblacs -lmpiblacsCinit -lmpiblacs
+
+SUPERLU_LIBS = -L$(SUPERLU_HOME)/lib -lsuperlu_4.2 \
+	-L$(SUPERLU_DIST_HOME)/lib -lsuperlu_dist_2.5 \
+
+PARMETIS_LIBS = -L$(PARMETIS_HOME)/lib \
+	-Wl,-rpath,$(PARMETIS_HOME)/lib -lparmetis -lmetis
+
+
+LIBS = 	$(PETSC_LIBS) \
+	$(SUPERLU_LIBS) \
+	$(PARMETIS_LIBS) \
+	-L$(Zoltan_HOME)/lib -lzoltan \
+	-L$(HDF5_HOME)/lib -lhdf5_fortran -lhdf5 \
+	-L$(FFTWHOME)/lib -lfftw3 -lfftw3_mpi -lfftw3_threads \
+	-L$(CCHOME)/mkl/lib/em64t -lmkl -lmkl_lapack \
+	-L$(CCHOME)/lib/intel64 -lguide \
+	-L$(NCARG_ROOT)/lib -lncarg -lncarg_gks -lncarg_c \
+	-Wl,-rpath -Wl,$(HDF5_HOME)/lib \
+	-L$(ZLIB_HOME) -lz \
+	-L/usr/X11R6/lib -lX11
+
+ifeq ($(USESCOREC), 1)
+
+  ifeq ($(USERW), 1)
+    SCORECDIR = /p/tsc/m3dc1/lib/SCORECLib/lib/Stix/031611
+    INCLUDE := -I/p/tsc/m3dc1/lib/SCORECLib/include/Stix/031611 \
+	$(INCLUDE)
+  else
+    SCORECDIR = /p/tsc/m3dc1/lib/SCORECLib/lib/Greene/120711
+    INCLUDE := -I/p/tsc/m3dc1/lib/SCORECLib/include/Greene/120711/ \
+        $(INCLUDE)
+
+  endif
+
+  SCOREC_ARCH=x86_64_linux-icc
+  SCOREC_LIBS = \
+	-L$(SCORECDIR) \
+	-Wl,-rpath,$(SCORECDIR) \
+	-lFMDB-openmpi$(SCORECOPT) \
+	-lSCORECModel-openmpi$(SCORECOPT) \
+	-lSCORECUtil-openmpi$(SCORECOPT) \
+	-lField-openmpi$(SCORECOPT) \
+	-lCore-openmpi$(SCORECOPT) \
+	-lmeshAdapt-openmpi$(SCORECOPT) \
+	-lmeshTools-openmpi$(SCORECOPT) \
+	-lSolver-openmpi$(SCORECOPT) \
+	-lPPPL-openmpi$(SCORECOPT) \
+	-lipcomman-openmpi$(SCORECOPT)
+#	-lPPPLPetscDEV-openmpi$(SCORECOPT) \
+
+  LIBS := $(SCOREC_LIBS) $(LIBS)
+
+endif   # on USESCOREC
+
+%.o : %.c
+	$(CC)  $(CCOPTS) $(INCLUDE) $< -o $@
+
+%.o : %.cpp
+	$(CPP)  $(CCOPTS) $(INCLUDE) $< -o $@
+
+%.o: %.f
+	$(F77) $(F77OPTS) $(INCLUDE) $< -o $@
+
+%.o: %.F
+	$(F77) $(F77OPTS) $(INCLUDE) $< -o $@
+
+%.o: %.f90
+	$(F90) $(F90OPTS) $(INCLUDE) -fpic $< -o $@
