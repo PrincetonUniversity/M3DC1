8851ba927dbb74697fcb6cda361b75cee69053cd
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2012-06-03 01:26:35 +0000

    * Updated fortran example

--- fusion_io/example_fortran.f90
+++ fusion_io/example_fortran.f90
@@ -1,49 +1,49 @@
 program fio_example
   use fusion_io
 
-  integer :: isrc, isrc2
-  integer :: pressure, density, magnetic_field, magnetic_field2
-  integer :: compound_field
-  integer :: i, npts
-  real :: R0, R1, Z0, Z1, phi0, phi1
-  integer :: ierr
+  ! Open namelist
+  integer, parameter :: max_files = 20
+  character(len=64) :: filename(max_files)
+  integer :: file_type(max_files)
+  real :: factor
+  integer :: timeslice, nfiles
 
+  integer, dimension(max_files) :: isrc, ipres, idens, imag
   real :: p(1), n(1), b(3), x(3)
+  real :: t1(1), t3(3)
 
-  ! Open a M3DC1 source
-  call fio_open_source_f(FIO_M3DC1_SOURCE, "C1.h5", isrc, ierr)
-  if(ierr.ne.0) return
-
-  ! Set options appropriate to this source
-  call fio_get_options_f(isrc, ierr)
-  call fio_set_int_option_f(FIO_TIMESLICE, 1, ierr)
-  call fio_set_int_option_f(FIO_PERTURBED_ONLY, 1, ierr)
-  call fio_set_real_option_f(FIO_LINEAR_SCALE, 10., ierr)
-
-  ! read fields
-  call fio_get_field_f(isrc, FIO_PRESSURE, pressure, ierr);
-  call fio_get_field_f(isrc, FIO_DENSITY, density, ierr);
-  call fio_get_field_f(isrc, FIO_MAGNETIC_FIELD, magnetic_field, ierr);
-
+  integer, parameter ::  npts = 10
+  real :: R0, R1, Z0, Z1, phi0, phi1
+  integer :: i, j, ierr
   
-  ! Open a second source
-  call fio_open_source_f(FIO_M3DC1_SOURCE, "C2.h5", isrc2, ierr)
-  if(ierr.ne.0) return
-  ! Set options appropriate to this source
-  call fio_get_options_f(isrc2, ierr)
-  call fio_set_int_option_f(FIO_TIMESLICE, 0, ierr)
-  call fio_set_int_option_f(FIO_PERTURBED_ONLY, 1, ierr)
-  call fio_set_real_option_f(FIO_LINEAR_SCALE, 10., ierr)
-  ! read fields
-  call fio_get_field_f(isrc2, FIO_MAGNETIC_FIELD, magnetic_field2, ierr);
+  namelist /fio_example_data/ filename, timeslice, nfiles, factor, file_type
+
+  ! Read namelist
+  open(unit=10, file='example_fortran.input')
+  read(10,nml=fio_example_data)
+  close(10)
+
+  ! read files and fields
+  do i=1, nfiles
+     print *, 'Reading ', filename(i)
+     call fio_open_source_f(file_type(i), trim(filename(i)), isrc(i), ierr)
+     if(ierr.ne.0) return
+
+     ! Set options appropriate to this source
+     call fio_get_options_f(isrc(i), ierr)
+     call fio_set_int_option_f(FIO_TIMESLICE, timeslice, ierr)
+     call fio_set_real_option_f(FIO_LINEAR_SCALE, factor, ierr)
+
+     ! For first file, read full fields (equilibrium + perturbed)
+     ! For subsequent file, read only perturbed parts
+     if(i.gt.1) call fio_set_int_option_f(FIO_PERTURBED_ONLY, 1, ierr)
+
+     ! read fields
+     call fio_get_field_f(isrc(i), FIO_PRESSURE, ipres(i), ierr);
+     call fio_get_field_f(isrc(i), FIO_DENSITY, idens(i), ierr);
+     call fio_get_field_f(isrc(i), FIO_MAGNETIC_FIELD, imag(i), ierr);
+  end do
  
-  ! Create a compound field
-  call fio_create_compound_field_f(compound_field, ierr)
-  call fio_add_field_f(compound_field, magnetic_field,  FIO_ADD,  1., ierr)
-  call fio_add_field_f(compound_field, magnetic_field2, FIO_ADD, -1., ierr)
-  
-
-  npts = 10;
   R0 = 1.6;
   R1 = 2.1;
   Z0 = 0.0;
@@ -58,28 +58,29 @@ program fio_example
 
      write(*, '("(",3F12.4,"):")') x
 
-     call fio_eval_field_f(pressure, x, p, ierr)
+     p = 0.
+     n = 0.
+     b = 0.
+
+     do j=1, nfiles
+        call fio_eval_field_f(ipres(j), x, t1, ierr)
+        print *, t1
+        p = p + t1
+        call fio_eval_field_f(idens(j), x, t1, ierr)
+        n = n + t1
+        call fio_eval_field_f(imag(j), x, t3, ierr)
+        b = b + t3
+     end do
+
      write(*, '("        pressure = ",1pE12.4)') p
-
-     call fio_eval_field_f(density, x, n, ierr)
      write(*, '("        density = ",1pE12.4)') n
-
-     call fio_eval_field_f(magnetic_field, x, b, ierr)
      write(*, '("        final field = ",1p3E12.4)') b
-
-     call fio_eval_field_f(magnetic_field2, x, b, ierr)
-     write(*, '("        intial field = ",1p3E12.4)') b
-
-     call fio_eval_field_f(compound_field, x, b, ierr)
-     write(*, '("        final-initial = ",1p3E12.4)') b
   end do
 
-  call fio_close_field_f(density, ierr)
-  call fio_close_field_f(pressure, ierr)
-  call fio_close_field_f(magnetic_field, ierr)
-  call fio_close_source_f(isrc, ierr)
-
-  call fio_close_field_f(magnetic_field2, ierr)
-  call fio_close_source_f(isrc2, ierr)
-
+  do i=1, nfiles
+     call fio_close_field_f(idens(i), ierr)
+     call fio_close_field_f(ipres(i), ierr)
+     call fio_close_field_f(imag(i), ierr)
+     call fio_close_source_f(isrc(i), ierr)
+  end do
 end program fio_example
