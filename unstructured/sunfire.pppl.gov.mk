ec3dcdc070d4e436c3b358a4e0c668be19d74d8f
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2014-02-12 19:34:49 +0000

    * Added capability to have pressure gradient outside Psi_norm = 1 in GS solve * Changed how GS vertical / horizontal stability works (no longer proportional to coil current)

--- unstructured/sunfire.pppl.gov.mk
+++ unstructured/sunfire.pppl.gov.mk
@@ -2,8 +2,8 @@ FOPTS = -c -r8 -implicitnone -fpp -warn all -DPetscDEV -DPETSC_31 $(OPTS)
 CCOPTS  = -c -O -DPetscDEV -DPETSC_31 #-DCJ_MATRIX_DUMP -DUSEHYBRID 
 
 ifeq ($(OPT), 1)
-  FOPTS  := $(FOPTS) # -fast
-  CCOPTS := $(CCOPTS) -O
+  FOPTS  := $(FOPTS) -fast -vec-report=0
+  CCOPTS := $(CCOPTS) -O -vec-report=0
 else
   FOPTS := $(FOPTS) -g -check all -check noarg_temp_created -debug all -ftrapuv
 endif
