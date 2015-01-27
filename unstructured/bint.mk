88983859f2214bc1efa7ad1625415609eefc60ca
jchensw <jchen@pppl.gov>
2015-01-27 16:44:18 +0000

    makefile for babbage and greene

---
+++ unstructured/bint.mk
@@ -0,0 +1,92 @@
+ifeq ($(TAU), 1)
+  TAU_OPTIONS = -optCPPOpts=-DUSETAU -optVerbose -optPreProcess -optMpi -optTauSelectFile=select.tau
+  CPP    = tau_cxx.sh $(TAU_OPTIONS)
+  CC     = tau_cc.sh  $(TAU_OPTIONS)
+  F90    = tau_f90.sh $(TAU_OPTIONS)
+  F77    = tau_f90.sh $(TAU_OPTIONS)
+  LOADER = tau_f90.sh $(TAU_OPTIONS)
+else
+  CPP = mpiicpc -DMPICH_IGNORE_CXX_SEEK -mmic
+  CC = mpiicc -mmic
+  F90 = mpiifort -mmic -align array64byte
+  F77 = mpiifort -mmic -align array64byte
+  LOADER = mpiifort -mmic -align array64byte
+endif
+
+ifeq ($(HPCTK), 1)
+  OPTS := $(OPTS) -gopt
+  LOADER := hpclink $(LOADER)
+endif
+
+    SCORECDIR = /chos/global/project/projectdirs/mp288/babbage/scorec
+ifeq ($(COM), 1)
+    SCORECLIB= -Wl,--start-group,-rpath,$(SCORECDIR)/lib -L$(SCORECDIR)/lib -lm3dc1_scorec_complex -lpcu -lgmi -lapf -lmds -lspr -lapf_zoltan -lzoltan -lparma -lma -lph -Wl,--end-group 
+    PETSC_DIR= /usr/common/usg/petsc/3.5.2-fee0b69/complex
+    PETSC_ARCH= knc_201501
+    PETSCLIB= -Wl,--start-group,-rpath,$(PETSC_DIR)/$(PETSC_ARCH)/lib -L$(PETSC_DIR)/$(PETSC_ARCH)/lib -lcmumps -ldmumps -lmumps_common -lpetsc -lpord -lscalapack -lsmumps -lsuperlu_4.3 -lsuperlu_dist_3.3 -lzmumps -lparmetis -lmetis -Wl,--end-group
+
+else
+    SCORECLIB= -Wl,--start-group,-rpath,$(SCORECDIR)/lib -L$(SCORECDIR)/lib -lm3dc1_scorec -lpcu -lgmi -lapf -lmds -lspr -lapf_zoltan -lzoltan -lparma -lma -lph -Wl,--end-group
+    PETSC_DIR= /usr/common/usg/petsc/3.5.2-fee0b69/real
+    PETSC_ARCH= knc_201501
+    PETSCLIB = -Wl,--start-group,-rpath,$(PETSC_DIR)/$(PETSC_ARCH)/lib -L$(PETSC_DIR)/$(PETSC_ARCH)/lib -lcmumps -ldmumps -lmumps_common -lpetsc -lpord -lscalapack -lsmumps -lsuperlu_4.3 -lsuperlu_dist_3.3 -lzmumps -lparmetis -lmetis -Wl,--end-group
+endif
+
+  INCLUDE := -I$(SCORECDIR)/include
+  LIBS := $(LIBS) \
+	$(SCORECLIB) \
+	$(PETSCLIB) \
+        -L$(MKLROOT)/lib/mic/ -lmkl_blacs_intelmpi_lp64 -lmkl_blas95_lp64 -lmkl_cdft_core -lmkl_core -lmkl_intel_lp64 -lmkl_lapack95_lp64 -lmkl_scalapack_lp64 -lmkl_sequential \
+        -lstdc++
+
+AUX = d1mach.o i1mach.o r1mach.o fdump.o dbesj0.o dbesj1.o
+
+OPTS := $(OPTS) -DPetscDEV -DKSPITS -DxUSEADIOS #-DUSEHYBRID -DCJ_MATRIX_DUMP
+
+#only define them if adios-1.3 is used; otherwise use hopper default
+#ADIOS_DIR=/usr/common/usg/adios/1.4.1
+#ADIOS_FLIB = -L${ADIOS_DIR}/lib -ladiosf_v1 -ladiosreadf_v1 \
+#             -L/usr/common/usg/minixml/2.7/lib -lm -lmxml \
+#             -L/usr/lib64/ -llustreapi
+
+INCLUDE := $(INCLUDE) -I$(HDF5_PAR_DIR)/include -I$(FFTW_DIR)/include \
+	-I$(PETSC_DIR)/$(PETSC_ARCH)/include -I$(PETSC_DIR)/include \
+	-I$(GSL_DIR)/include
+
+LIBS := $(LIBS) \
+	-Wl,--start-group,-rpath,$(HDF5_PAR_DIR)/lib -L$(HDF5_PAR_DIR)/lib -lhdf5hl_fortran -lhdf5_fortran -lhdf5_hl -lhdf5 -ldl -lstdc++ -ldl -Wl,--end-group \
+	-L$(FFTW_DIR)/lib -lfftw3 \
+	-L$(GSL_DIR)/lib -lgsl -lgslcblas
+
+FOPTS = -c -r8 -implicitnone -fpp -warn all $(OPTS) \
+	-Dglobalinsertval=insertval -Dglobalentdofs=entdofs
+CCOPTS  = -c -O $(OPTS)
+
+# Optimization flags
+ifeq ($(OPT), 1)
+  LDOPTS := $(LDOPTS)
+  FOPTS  := $(FOPTS)  -O0
+  CCOPTS := $(CCOPTS)
+else
+  FOPTS := $(FOPTS) -g -Mbounds
+  CCOPTS := $(CCOPTS)  
+endif
+
+
+F90OPTS = $(F90FLAGS) $(FOPTS)
+F77OPTS = $(F77FLAGS) $(FOPTS)
+
+%.o : %.cpp
+	$(CPP)  $(CCOPTS) $(INCLUDE) $< -o $@
+
+%.o : %.c
+	$(CC)  $(CCOPTS) $(INCLUDE) $< -o $@
+
+%.o: %.f
+	$(F77) $(F77OPTS) $(INCLUDE) $< -o $@
+
+%.o: %.F
+	$(F77) $(F77OPTS) $(INCLUDE) $< -o $@
+
+%.o: %.f90
+	$(F90) $(F90OPTS) $(INCLUDE) $< -o $@
--- unstructured/greene.pppl.gov.mk
+++ unstructured/greene.pppl.gov.mk
@@ -35,32 +35,48 @@ HYBRID_HOME = /p/swim/jchen/pdslin_0.0
 HYBRID_LIBS = -L$(HYBRID_HOME)/lib -lhsolver
 
 INCLUDE = -I$(MPIHOME)/include \
-	-I$(PETSC_DIR)/include -I$(PETSC_DIR)/$(PETSC_ARCH)/include \
-	-I$(GSLHOME)/include \
-	-I$(HYBRID_HOME)/include
+	  -I$(PETSC_DIR)/include -I$(PETSC_DIR)/$(PETSC_ARCH)/include \
+	  -I$(PARMETIS_HOME)/include \
+	  -I$(FFTWHOME)/include \
+	  -I$(SUPERLU_DIST_HOME)/include -I$(SUPERLUHOME)/include \
+	  -I$(BLACS_HOME)/include \
+	  -I$(Zoltan_HOME)/include \
+	  -I$(GSLHOME)/include \
+	  -I$(HDF5_HOME)/include \
+	  -I$(NCARG_ROOT)/include
 
 #	-L$(MUMPS_HOME)/lib -ldmumps -lmumps_common -lpord \
 #	-L$(SCALAPACK_HOME) -lscalapack \
 #	-L$(BLACS_HOME)/lib -lmpiblacsF77init -lmpiblacs -lmpiblacsCinit -lmpiblacs
 
-LIBS = 	$(PETSC_LIBS) \
+LIBS = 	-L$(PETSC_DIR)/$(PETSC_ARCH)/lib -lmumps_common -ldmumps -lcmumps -lzmumps -lsmumps -lpord -lpetsc \
+        -L$(PARMETIS_HOME)/lib -lparmetis -lmetis \
 	-L$(FFTWHOME)/lib -lfftw3 -lfftw3_mpi -lfftw3_threads \
+	-L$(SUPERLU_DIST_HOME)/lib -lsuperlu_dist_3.3 -L$(SUPERLUHOME)/lib -lsuperlu_4.3 \
+	-L$(SCALAPACK_HOME)/lib -lscalapack -L$(BLACS_HOME)/lib -lmpiblacs -lmpiblacsCinit -lmpiblacsF77init \
+        -L$(Zoltan_HOME)/lib -lzoltan \
+	-L$(GSLHOME)/lib -lgsl \
+        -L$(HDF5_HOME)/lib -lhdf5_fortran -lhdf5_hl -lhdf5 \
+	-L$(NCARG_ROOT)/lib -lncarg -lncarg_gks -lncarg_c \
         -L$(CCHOME)/mkl/lib/em64t -lmkl -lmkl_lapack \
 	-L$(CCHOME)/lib/intel64 -lguide \
-	-L$(NCARG_ROOT)/lib -lncarg -lncarg_gks -lncarg_c \
-	-L$(GSLHOME)/lib -lgsl \
 	-L/usr/X11R6/lib -lX11
 
-ifeq ($(USESCOREC), 1)
-  #SCORECDIR = /p/tsc/m3dc1/lib/develop.petsc3.Fan/build_greene/cmake
-  SCORECDIR=/p/tsc/m3dc1/lib/develop.petsc3.Fan/build_greene/newpetsc
-  SCORECLIB=-lapf -lapf_pumi -lpumi_util -lpumi_geom -lpcu -lpumi_geom_meshmodel -lpumi_mesh -lmeshadapt
-  SCOREC_LIBS = -Wl,-rpath,$(SCORECDIR)/lib -L$(SCORECDIR)/lib -Wl,--start-group -lPPPLFusion $(SCORECLIB) -Wl,--end-group -lzoltan
- PETSC_EXTERNAL_LIB_BASIC = -Wl,-rpath,/p/tsc/m3dc1/lib/SCORECLib/petsc-3.5.1-greene/petsc-3.5.1/intel-ompi/lib -L/p/tsc/m3dc1/lib/SCORECLib/petsc-3.5.1-greene/petsc-3.5.1/intel-ompi/lib -lsuperlu_dist_3.3 -lcmumps -ldmumps -lsmumps -lzmumps -lmumps_common -lpord -lscalapack -Wl,-rpath,/usr/pppl/intel/11-pkgs/acml-4.3.0/ifort64/lib -L/usr/pppl/intel/11-pkgs/acml-4.3.0/ifort64/lib -lacml -lparmetis -lmetis -lX11 -lpthread -lssl -lcrypto -lhdf5_fortran -lhdf5_hl -lhdf5 -Wl,-rpath,/usr/pppl/intel/11-pkgs/openmpi-1.4.1/lib -L/usr/pppl/intel/11-pkgs/openmpi-1.4.1/lib -Wl,-rpath,/usr/pppl/intel/11.0/081/lib/intel64 -L/usr/pppl/intel/11.0/081/lib/intel64 -Wl,-rpath,/usr/lib/gcc/x86_64-redhat-linux/4.1.2 -L/usr/lib/gcc/x86_64-redhat-linux/4.1.2 -lmpi_f90 -lmpi_f77 -lifport -lifcore -lm -lmpi_cxx -lstdc++ -lmpi_cxx -lstdc++ -ldl -lmpi -lopen-rte -lopen-pal -lnsl -lutil -limf -lsvml -lipgo -ldecimal -lirc -lgcc_s -lpthread -lirc_s -ldl
-  LIBS := $(SCOREC_LIBS) -L$(PETSC_DIR)/$(PETSC_ARCH)/lib $(PETSC_EXTERNAL_LIB_BASIC) -lpetsc $(LIBS)
+  SCORECDIR=/p/tsc/m3dc1/lib/SCORECLib/greene/latest
+ifeq ($(COM), 1)
+  SCORECLIB= -Wl,--start-group,-rpath,$(SCORECDIR)/lib -L$(SCORECDIR)/lib \
+             -lapf -lgmi -lma -lparma -lph -lmds -lpcu -lspr -lapf_zoltan -lm3dc1_scorec_complex \
+             -Wl,--start-group
+else
+  SCORECLIB= -Wl,--start-group,-rpath,$(SCORECDIR)/lib -L$(SCORECDIR)/lib \
+             -lapf -lgmi -lma -lparma -lph -lmds -lpcu -lspr -lapf_zoltan -lm3dc1_scorec \
+             -Wl,--start-group
+endif
+
+  LIBS := $(SCORECLIB) \
+	$(LIBS) 
   INCLUDE := -I$(SCORECDIR)/include \
         $(INCLUDE)
-endif   # on USESCOREC
 
 %.o : %.c
 	$(CC)  $(CCOPTS) $(INCLUDE) $< -o $@
