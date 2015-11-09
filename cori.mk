74f3279465567573937fbb5efbe38485b591ccae
seegyoung <seols@rpi.edu>
2015-11-09 17:28:06 +0000

    change in unstructured - adding readme and .mk for cori and scorec

---
+++ cori.mk
@@ -0,0 +1,106 @@
+ifeq ($(TAU), 1)
+  TAU_OPTIONS = -optCPPOpts=-DUSETAU -optVerbose -optPreProcess -optMpi -optTauSelectFile=select.tau
+  CPP    = tau_cxx.sh $(TAU_OPTIONS)
+  CC     = tau_cc.sh  $(TAU_OPTIONS)
+  F90    = tau_f90.sh $(TAU_OPTIONS)
+  F77    = tau_f90.sh $(TAU_OPTIONS)
+  LOADER = tau_f90.sh $(TAU_OPTIONS)
+else
+  CPP = CC
+  CC = cc
+  F90 = ftn
+  F77 = ftn
+  LOADER = ftn
+endif
+
+ifeq ($(HPCTK), 1)
+  OPTS := $(OPTS) -gopt
+  LOADER := hpclink $(LOADER)
+endif
+
+# define where you want to locate the mesh adapt libraries
+#HYBRID_HOME =  /scratch2/scratchdirs/xyuan/Software_Hopper/pdslin_0.0
+#HYBRID_LIBS = -L$(HYBRID_HOME)/lib -lpdslin
+SCOREC_DIR = /global/project/projectdirs/mp288/cori/scorec/Nov2015
+SCOREC_CORE = -lapf -lgmi -lma -lparma -lph -lapf_zoltan -lmds -lpcu -lspr
+HDF5_DIR = /opt/cray/hdf5-parallel/1.8.14/INTEL/14.0
+
+ifeq ($(COM), 1)
+      SCOREC_LIBS=-L$(SCOREC_DIR)/lib -Wl,--start-group $(SCOREC_CORE) -lm3dc1_scorec_complex -Wl,--end-group
+      PETSC_LIB = -lcraypetsc_intel_complex
+else
+      SCOREC_LIBS=-L$(SCOREC_DIR)/lib -Wl,--start-group $(SCOREC_CORE) -lm3dc1_scorec -Wl,--end-group
+      PETSC_LIB = -lcraypetsc_intel_real
+endif
+INCLUDE := $(INCLUDE) -I$(SCOREC_DIR)/include
+
+PETSC_EXTERNAL_LIB_BASIC = -Wl,-rpath,$(PETSC_DIR)/$(PETSC_ARCH)/lib -L/opt/cray/tpsl/1.4.3/INTEL/140/sandybridge/lib -lHYPRE -lsuperlu -lcmumps -ldmumps -lesmumps -lsmumps -lzmumps -lmumps_common -lptesmumps -lpord -lsuperlu_dist -lparmetis -lmetis -lptscotch -lscotch -lptscotcherr -lscotcherr -lsci_intel_mpi_mp -lsci_intel_mp -liomp5 -lsundials_cvode -lsundials_cvodes -lsundials_ida -lsundials_idas -lsundials_kinsol -lsundials_nvecparallel -lsundials_nvecserial -lpthread -lssl -lcrypto -lstdc++
+INCLUDE := $(INCLUDE) -I$(PETSC_DIR)/include
+
+LIBS := $(LIBS) \
+        -L$(SCOREC_DIR)/lib $(SCOREC_LIBS) \
+        -L$(PETSC_DIR)/$(PETSC_ARCH)/lib $(PETSC_LIB) $(PETSC_EXTERNAL_LIB_BASIC) \
+        -L$(CRAY_TRILINOS_PREFIX)/lib -lzoltan \
+        -lstdc++
+
+ifeq ($(USEADIOS), 1)
+  OPTS := $(OPTS) -DUSEADIOS
+endif
+
+AUX = d1mach.o i1mach.o r1mach.o fdump.o dbesj0.o dbesj1.o
+
+OPTS := $(OPTS) -DPetscDEV -DKSPITS #-DUSEHYBRID -DCJ_MATRIX_DUMP
+
+#only define them if adios-1.3 is used; otherwise use hopper default
+#ADIOS_DIR=/global/homes/p/pnorbert/adios/hopper
+#ADIOS_DIR=/global/homes/p/pnorbert/adios/1.3.1/hopper/pgi/
+#ADIOS_FLIB = -L${ADIOS_DIR}/lib -ladiosf -L/global/homes/p/pnorbert/mxml/mxml.hopper/lib -lm -lmxml -llustreapi -pgcpplibs
+ADIOS_DIR=/usr/common/usg/adios/1.4.1
+ADIOS_FLIB = -L${ADIOS_DIR}/lib -ladiosf_v1 -ladiosreadf_v1 \
+             -L/usr/common/usg/minixml/2.7/lib -lm -lmxml \
+             -L/usr/lib64/ -llustreapi
+
+INCLUDE := $(INCLUDE) -I$(HDF5_DIR)/include $(FFTW_INCLUDE_OPTS) \
+	-I$(PETSC_DIR)/$(PETSC_ARCH)/include -I$(PETSC_DIR)/include \
+	-I$(GSL_DIR)/include # \
+#        -I$(HYBRID_HOME)/include
+
+LIBS := $(LIBS) -L$(HDF5_DIR)/lib -lhdf5_fortran -lhdf5_hl -lhdf5 -lz \
+	$(FFTW_POST_LINK_OPTS) -lfftw3 \
+	-L$(GSL_DIR)/lib -lgsl -lhugetlbfs \
+	$(ADIOS_FLIB)
+#        $(HYBRID_LIBS) \
+
+
+FOPTS = -c -r8 -implicitnone -fpp -warn all $(OPTS) \
+	-Dglobalinsertval=insertval -Dglobalentdofs=entdofs
+CCOPTS  = -c $(OPTS)
+
+# Optimization flags
+ifeq ($(OPT), 1)
+  LDOPTS := $(LDOPTS)
+  FOPTS  := $(FOPTS)  -O3
+  CCOPTS := $(CCOPTS) -O3
+else
+  FOPTS := $(FOPTS) -g -Mbounds -check all -fpe0 -warn -traceback -debug extended
+  CCOPTS := $(CCOPTS)  
+endif
+
+
+F90OPTS = $(F90FLAGS) $(FOPTS)
+F77OPTS = $(F77FLAGS) $(FOPTS)
+
+%.o : %.cpp
+	$(CPP)  $(CCOPTS) $(INCLUDE) $< -o $@
+
+%.o : %.c
+	$(CC)  $(CCOPTS) $(INCLUDE) $< -o $@
+
+%.o: %.f
+	$(F77) $(F77OPTS) $(INCLUDE) $< -o $@
+
+%.o: %.F
+	$(F77) $(F77OPTS) $(INCLUDE) $< -o $@
+
+%.o: %.f90
+	$(F90) $(F90OPTS) $(INCLUDE) $< -o $@
