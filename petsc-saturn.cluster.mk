77e15184e29f3d31c324e15cabf2815646cd1783
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2015-12-18 16:08:35 +0000

    * Changed compiler options to ensure that double precision is 8 bytes on   saturn

--- petsc-saturn.cluster.mk
+++ petsc-saturn.cluster.mk
@@ -7,7 +7,7 @@ LOADER = mpif90
 AUX = d1mach.o i1mach.o r1mach.o fdump.o dbesj0.o dbesj1.o
 
 OPTS := $(OPTS) -DPetscDEV -DKSPITS
-FOPTS = -c -cpp -fdefault-real-8 -Wall -static $(OPTS) \
+FOPTS = -c -cpp -fdefault-real-8 -fdefault-double-8 -Wall -static $(OPTS) \
         -Wno-unused-variable -ffree-line-length-0 -Wno-unused-dummy-argument \
         -Dglobalinsertval=insertval -Dglobalentdofs=entdofs
 
