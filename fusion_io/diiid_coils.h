fe86efeefe36506128c9208958cc37270145a261
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2016-01-04 22:35:28 +0000

    * Removed fusion_io libraries from svn repository; these have been moved to   github (https://github.com/nferraro/fusion-io)

--- fusion_io/diiid_coils.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIIID_COIL_SOURCE_H
-#define DIIID_COIL_SOURCE_H
-
-#include "coil_source.h"
-
-void diiid_icoils(coil_source*, const double current, const int n, 
-		  const double top_phase, const double bottom_phase);
-
-#endif
