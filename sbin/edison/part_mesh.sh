7d08cbc02515e6f0f514a7ebe7d62d8f41ec5be8
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2016-01-19 15:53:58 +0000

    * Bugfix to install_doc target * Updated edison batch scripts to use SLURM

--- sbin/edison/part_mesh.sh
+++ sbin/edison/part_mesh.sh
@@ -13,14 +13,14 @@ else
     if [ ${1: -4} == ".sms" ]; then
 	echo "Splitting .sms file"
 	echo "*******************"
-	aprun -n 1 $CONVERT $1 $2 $MESH_FILE
-	aprun -n 1 $MAKE_MODEL $MESH_FILE $MODEL_FILE
-	aprun -n $3 $SPLIT_SMB $MODEL_FILE $MESH_FILE part.smb $3
+	srun -n 1 $CONVERT $1 $2 $MESH_FILE
+	srun -n 1 $MAKE_MODEL $MESH_FILE $MODEL_FILE
+	srun -n $3 $SPLIT_SMB $MODEL_FILE $MESH_FILE part.smb $3
     elif [ ${1: -4} == ".smb" ]; then
 	echo "Splitting .smb file"
 	echo "*******************"
-	aprun -n 1 $MAKE_MODEL $1 $MODEL_FILE
-	aprun -n $2 $SPLIT_SMB $MODEL_FILE $1 part.smb $2
+	srun -n 1 $MAKE_MODEL $1 $MODEL_FILE
+	srun -n $2 $SPLIT_SMB $MODEL_FILE $1 part.smb $2
     fi
 fi
 
