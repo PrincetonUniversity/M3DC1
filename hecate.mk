c1367c2c803d59a1455c0321c3f3f8a09381d90f
seegyoung <seols@rpi.edu>
2016-02-29 18:57:57 +0000

    change in unstructured - updating hecate makefile and readme

--- hecate.mk
+++ hecate.mk
@@ -27,53 +27,45 @@ endif
 F90OPTS = $(F90FLAGS) $(FOPTS) -gen-interfaces
 F77OPTS = $(F77FLAGS) $(FOPTS)
 
+SCOREC_HOME=/home/jinchen/lib/scorec/Mar2016
+ifeq ($(COM), 1)
+    M3DC1_SCOREC_LIB = m3dc1_scorec_complex
+else
+    M3DC1_SCOREC_LIB = m3dc1_scorec
+    HYPRE = -lHYPRE
+endif
+
+SCOREC_LIBS= -Wl,--start-group,-rpath,$(SCOREC_HOME)/lib -L$(SCOREC_HOME)/lib \
+    -l$(M3DC1_SCOREC_LIB) \
+    -lcrv -ldsp -lph -lsize -lsam -lspr -lma -lapf_zoltan -lparma -lmds -lapf -llion -lmth -lgmi -lpcu \
+    -Wl,--end-group
 
 # define petsc and other libs
-	PETSC_DIR = /home/jinchen/lib/petsc-3.5.3
+PETSC_DIR = /home/jinchen/lib/petsc-3.5.3
 ifeq ($(COM), 1)
 	PETSC_ARCH = hecate-intel-mt-complex
 else
 	PETSC_ARCH = hecate-intel-mt
 endif
 
-INCLUDE = -I$(MPIHOME)/include \
-	-I$(PETSC_DIR)/include -I$(PETSC_DIR)/$(PETSC_ARCH)/include \
-	-I$(HDF5DIR)/include -I$(HDF5DIR)/lib64 \
-
-ifeq ($(COM), 1)
 PETSCLIBS = \
         -lpetsc \
+        $(HYPRE) \
         -lzoltan \
         -lfftw3_mpi -lfftw3 \
         -lmetis -lparmetis \
         -lsuperlu_4.3 -lsuperlu_dist_3.3 \
         -ldmumps -lmumps_common -lcmumps -lzmumps -lpord
-else
-PETSCLIBS = \
-        -lpetsc \
-        -lHYPRE \
-        -lzoltan \
-        -lfftw3_mpi -lfftw3 \
-        -lmetis -lparmetis \
-        -lsuperlu_4.3 -lsuperlu_dist_3.3 \
-        -ldmumps -lmumps_common -lcmumps -lzmumps -lpord
-endif
 
 BLASLAPACKLIBS = -L$(MKLROOT)/lib/intel64 -Wl,--start-group -lmkl_blacs_sgimpt_lp64 -lmkl_lapack95_lp64 -lmkl_blas95_lp64 -lmkl_intel_lp64 -lmkl_cdft_core -lmkl_scalapack_lp64 -lmkl_sequential -lmkl_core -Wl,--end-group
 
 PETSC_LIBS = -L$(PETSC_DIR)/$(PETSC_ARCH)/lib -Wl,--start-group $(PETSCLIBS) -Wl,--end-group
 
-SCORECDIR = /home/jinchen/lib/scorec/Dec2015
-INCLUDE := -I$(SCORECDIR)/include $(INCLUDE)
-ifeq ($(COM), 1)
-  SCORECLIB=-lapf -lgmi -lm3dc1_scorec_complex -lma -lparma -lph -lapf_zoltan -lmds -lpcu -lspr
-else
-  SCORECLIB=-lapf -lgmi -lm3dc1_scorec -lma -lparma -lph -lapf_zoltan -lmds -lpcu -lspr
-endif
-SCOREC_LIBS =-L$(SCORECDIR)/lib -Wl,--start-group $(SCORECLIB) -Wl,--end-group
+INCLUDE = -I$(MPIHOME)/include \
+        -I$(PETSC_DIR)/include -I$(PETSC_DIR)/$(PETSC_ARCH)/include \
+        -I$(SCOREC_HOME)/include -I$(HDF5DIR)/include -I$(HDF5DIR)/lib64
 
-LIBS = \
-	$(BLASLAPACKLIBS) \
+LIBS = $(BLASLAPACKLIBS) \
        $(SCOREC_LIBS) \
        $(PETSC_LIBS) \
         -L$(HDF5DIR)/lib64 -lhdf5_fortran -lhdf5 \
