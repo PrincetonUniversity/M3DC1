721bec4f19498797fd82bb4a29ab9b8f4a140052
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2016-04-26 19:07:20 +0000

    * Additions to m3dc1_units script

--- unstructured/sbin/m3dc1_units.sh
+++ unstructured/sbin/m3dc1_units.sh
@@ -1,53 +1,52 @@
 #!/bin/bash
 
-# Check if C1input exists
-if [ ! -f C1input ]; then
-    echo 'C1input not found.'
-    return
-fi
-
-# Parse C1input for normalizations
-BLINE=`grep b0_norm C1input`
-if [ $? == 0 ]; then
-    B0=`echo $BLINE | awk '{ print $3 }'`
-else
-    echo "b0_norm not found.  Using default value"
-    B0=1e4
-fi
-NLINE=`grep n0_norm C1input`
-if [ $? == 0 ]; then
-    N0=`echo $NLINE | awk '{ print $3 }'`
-else
-    echo "n0_norm not found.  Using default value"
-    N0=1e14
-fi
-LLINE=`grep l0_norm C1input`
-if [ $? == 0 ]; then
-    L0=`echo $LLINE | awk '{ print $3 }'`
-else
-    echo "l0_norm not found.  Using default value"
-    L0=100.
-fi
-MULINE=`grep ion_mass C1input`
-if [ $? == 0 ]; then
-    MU=`echo $MULINE | awk '{ print $3 }'`
-else
-    echo "ion_mass not found.  Using default value"
-    MU=1.
-fi
-ZLINE=`grep zeff C1input`
-if [ $? == 0 ]; then
-    ZEFF=`echo $ZLINE | awk '{ print $3 }'`
-else
-    echo "zeff not found.  Using default value"
-    ZEFF=1.
-fi
-
-
+B0=1e4
+N0=1e14
+L0=100.
+MU=1
+ZEFF=1
 PI=3.14159265359
 C=2.9979e10
 MP=1.6726e-24
 E=4.8032e-10
+
+# Check if C1input exists
+if [ ! -f C1input ]; then
+    echo 'WARNING: C1input file not found.  Using default values.'
+else
+
+    # Parse C1input for normalizations
+    BLINE=`grep b0_norm C1input`
+    if [ $? == 0 ]; then
+	B0=`echo $BLINE | awk '{ print $3 }'`
+    else
+	echo "b0_norm not found.  Using default value"
+    fi
+    NLINE=`grep n0_norm C1input`
+    if [ $? == 0 ]; then
+	N0=`echo $NLINE | awk '{ print $3 }'`
+    else
+	echo "n0_norm not found.  Using default value"
+    fi
+    LLINE=`grep l0_norm C1input`
+    if [ $? == 0 ]; then
+	L0=`echo $LLINE | awk '{ print $3 }'`
+    else
+	echo "l0_norm not found.  Using default value"
+    fi
+    MULINE=`grep ion_mass C1input`
+    if [ $? == 0 ]; then
+	MU=`echo $MULINE | awk '{ print $3 }'`
+    else
+	echo "ion_mass not found.  Using default value"
+    fi
+    ZLINE=`grep zeff C1input`
+    if [ $? == 0 ]; then
+	ZEFF=`echo $ZLINE | awk '{ print $3 }'`
+    else
+	echo "zeff not found.  Using default value"
+    fi
+fi
 
 B0_MKS=$(echo | awk "{ print $B0/1.e4 }" )
 N0_MKS=$(echo | awk "{ print $N0*1.e6 }" )
@@ -60,6 +59,8 @@ P0=$(echo - | awk "{ print $B0*$B0/(4.*$PI) }" )
 P0_MKS=$(echo - | awk "{ print $P0/10. }" )
 
 T0=$(echo - | awk "{ print $L0/$V0 }" )
+FREQ0=$(echo - | awk "{ print 1./$T0 }" )
+AFREQ0=$(echo - | awk "{ print 2*$PI*$FREQ0 }" )
 
 W0=$(echo - | awk "{ print $P0/$N0 }" )
 W0_MKS=$(echo - | awk "{ print $W0/1e7 }" )
@@ -71,6 +72,8 @@ echo "B0 = $B0 G = $B0_MKS (T)"
 echo "n0 = $N0 /cm^3 = $N0_MKS /m^3"
 echo "L0 = $L0 cm = $L0_MKS  m"
 echo "t0 = $T0 s"
+echo "1/t0 = $FREQ0 Hz"
+echo "2pi/t0 = $AFREQ0 rad/s"
 echo "v0 = $V0 cm/s = $V0_MKS m/s"
 echo "p0 = $P0 dyne/cm^2 = $P0_MKS pascal"
 echo "T0 = $W0_EV eV"
