c7cf976c9be68f038d31f58af05a8ad18db5d7e4
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2016-06-28 21:06:29 +0000

    * Fixed bugs in kappag implementation

--- unstructured/ludef_t.f90
+++ unstructured/ludef_t.f90
@@ -3251,28 +3251,28 @@ subroutine pressure_lin(trial, lin, ssterm, ddterm, q_ni, r_bf, q_bf,&
   ! Gradient-dependent heat flux
   if(kappag.ne.0) then
      if(linear.eq.0) then
-        temp = b3pedkappa(trial,lin,pp179,pp179,hp) &
-             + b3pedkappa(trial,pp179,lin,pp179,hp) &
-             + b3pedkappa(trial,pp179,pp179,lin,hp)
+        temp = b3pedkappag(trial,lin,pp179,pp179) &
+             + b3pedkappag(trial,pp179,lin,pp179) &
+             + b3pedkappag(trial,pp179,pp179,lin)
         ssterm(pp_g) = ssterm(pp_g) -        thimp     *dt*temp
         ddterm(pp_g) = ddterm(pp_g) + (1./3.-thimp*bdf)*dt*temp
         
-        if(eqsubtract.eq.0) then
-           temp = b3pedkappa(trial,lin,pp179,pp079,hp) &
-                + b3pedkappa(trial,pp179,lin,pp079,hp) &
-                + b3pedkappa(trial,pp179,pp079,lin,hp) &
-                + b3pedkappa(trial,lin,pp079,pp179,hp) &
-                + b3pedkappa(trial,pp079,lin,pp179,hp) &
-                + b3pedkappa(trial,pp079,pp179,lin,hp)
+        if(eqsubtract.eq.1) then
+           temp = b3pedkappag(trial,lin,pp179,pp079) &
+                + b3pedkappag(trial,pp179,lin,pp079) &
+                + b3pedkappag(trial,pp179,pp079,lin) &
+                + b3pedkappag(trial,lin,pp079,pp179) &
+                + b3pedkappag(trial,pp079,lin,pp179) &
+                + b3pedkappag(trial,pp079,pp179,lin)
            ssterm(pp_g) = ssterm(pp_g) -        thimp     *dt*temp
            ddterm(pp_g) = ddterm(pp_g) + (1./2.-thimp*bdf)*dt*temp     
         end if
      end if
 
      if(eqsubtract.eq.1) then
-        temp = b3pedkappa(trial,lin,pp079,pp079,hp) &
-             + b3pedkappa(trial,pp079,lin,pp079,hp) &
-             + b3pedkappa(trial,pp079,pp079,lin,hp)
+        temp = b3pedkappag(trial,lin,pp079,pp079) &
+             + b3pedkappag(trial,pp079,lin,pp079) &
+             + b3pedkappag(trial,pp079,pp079,lin)
         ssterm(pp_g) = ssterm(pp_g) -     thimp     *dt*temp
         ddterm(pp_g) = ddterm(pp_g) + (1.-thimp*bdf)*dt*temp     
      end if
