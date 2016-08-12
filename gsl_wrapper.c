a6c59adfbf471a423e68e18b601a4f504f8b1b48
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2016-08-12 23:19:23 +0000

    * Additional updates that should have been included in previous revision

--- gsl_wrapper.c
+++ gsl_wrapper.c
@@ -1,5 +1,6 @@
 #include <gsl/gsl_sf_bessel.h>
 #include <gsl/gsl_sf_erf.h>
+#include <gsl/gsl_sf_ellint.h>
 
 void gsl_bessel_i_(const int* n, const double* x, double *out)
 {
@@ -14,6 +15,16 @@ void gsl_bessel_j_(const int* n, const double* x, double *out)
 void gsl_bessel_y_(const int* n, const double* x, double *out)
 {
   *out = gsl_sf_bessel_Yn(*n, *x);
+}
+
+void gsl_elliptic_e_(const double* k, double *out)
+{
+  *out = gsl_sf_ellint_Ecomp(*k, GSL_PREC_SINGLE);
+}
+
+void gsl_elliptic_k_(const double* k, double *out)
+{
+  *out = gsl_sf_ellint_Kcomp(*k, GSL_PREC_SINGLE);
 }
 
 void gsl_erf_(const double* x, double *out)
