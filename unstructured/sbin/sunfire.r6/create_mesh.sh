2f0418bcf6d999c2dbced85c62661eb925b78b4d
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2016-08-14 20:37:32 +0000

    * Updated create_mesh.sh to use m3dc1_meshgen

--- unstructured/sbin/sunfire.r6/create_mesh.sh
+++ unstructured/sbin/sunfire.r6/create_mesh.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-POINTNAME=`awk '{ if($1 == "modelName")  print $2 }' input | tr '[:upper:]' '[:lower:]'`
-THICKNESS=`awk '{ if($1 == "thickness")  print $2 }' input`
-NAME=$POINTNAME$THICKNESS
-
-echo $NAME
-
-/p/tsc/m3dc1/lib/SCORECLib/rhel6/utilities/create_mesh/create_smd
-/p/tsc/m3dc1/lib/SCORECLib/rhel6/utilities/create_mesh/convert_sim_sms $NAME.smd $NAME.sms $NAME.smb
+/p/tsc/m3dc1/lib/SCORECLib/rhel6/utilities/m3dc1_meshgen/m3dc1_meshgen $1
