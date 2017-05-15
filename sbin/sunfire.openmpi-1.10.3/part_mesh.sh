9a701cd96a1f952102898b211e6550f25a516251
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2017-05-15 12:44:02 +0000

    * Updated sbin scripts for sunfire

---
+++ sbin/sunfire.openmpi-1.10.3/part_mesh.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CONVERT=/p/tsc/m3dc1/lib/SCORECLib/rhel6/openmpi-1.10.3/utilities/create_mesh/convert_sim_sms
+SPLIT_SMB=/p/tsc/m3dc1/lib/SCORECLib/rhel6/openmpi-1.10.3/utilities/split_smb/split_smb
+MAKE_MODEL=/p/tsc/m3dc1/lib/SCORECLib/rhel6/openmpi-1.10.3/utilities/split_smb/make_model
+MESH_FILE=mesh.smb
+MODEL_FILE=model.dmg
+
+if [ -z $1 ]; then
+    echo "Usage: part_mesh.sh <model.smd> <mesh.sms> <parts>"
+    echo "   or: part_mesh.sh <mesh.smb> <parts>"
+else
+    if [ ${1: -4} == ".smd" ]; then
+	echo "Splitting .sms file"
+	echo "*******************"
+	mpiexec -np 1 $CONVERT $1 $2 $MESH_FILE
+	mpiexec -np 1 $MAKE_MODEL $MESH_FILE $MODEL_FILE
+	mpiexec -np $3 $SPLIT_SMB $MODEL_FILE $MESH_FILE part.smb $3
+    elif [ ${1: -4} == ".smb" ]; then
+	echo "Splitting .smb file"
+	echo "*******************"
+	mpiexec -np 1 $MAKE_MODEL $1 $MODEL_FILE
+	mpiexec -np $2 $SPLIT_SMB $MODEL_FILE $1 part.smb $2
+    fi
+fi
+
--- sbin/sunfire.openmpi-1.8.4/create_mesh.sh
+++ sbin/sunfire.openmpi-1.8.4/create_mesh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/p/tsc/m3dc1/lib/SCORECLib/rhel6/utilities/m3dc1_meshgen/m3dc1_meshgen $1
+/p/tsc/m3dc1/lib/SCORECLib/rhel6/openmpi-1.8.4/utilities/m3dc1_meshgen/m3dc1_meshgen $1
