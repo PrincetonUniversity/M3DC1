222c231d96a9d2dd36a4bc9ff14ef50a7337eb77
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2017-08-09 15:57:03 +0000

    * Changed create_mesh.sh script to remove test on m3dc1_meshgen

--- unstructured/sbin/sunfire.openmpi-1.8.4/create_mesh.sh
+++ unstructured/sbin/sunfire.openmpi-1.8.4/create_mesh.sh
@@ -5,19 +5,19 @@ if [ ! -f input ]; then
     exit
 fi
 
-if hash m3dc1_meshgen 2>/dev/null; then
-    echo "m3dc1_meshgen is present."
+#if command -v m3dc1_meshgen 2>/dev/null; then
+#    echo "m3dc1_meshgen is present."
 
     m3dc1_meshgen input
-else
-    echo "m3dc1_meshgen is not present.  Trying create_smb."
-    POINTNAME=`awk '{ if($1 == "modelName")  print $2 }' input | tr '[:upper:]' '[:lower:]'`
-    THICKNESS=`awk '{ if($1 == "thickness")  print $2 }' input`
-    NAME=$POINTNAME$THICKNESS
-
-    echo $NAME
-
-    create_smb
-    convert_sim_sms $NAME.smd $NAME.sms $NAME.smb
-fi
+#else
+#    echo "m3dc1_meshgen is not present.  Trying create_smb."
+#    POINTNAME=`awk '{ if($1 == "modelName")  print $2 }' input | tr '[:upper:]' '[:lower:]'`
+#    THICKNESS=`awk '{ if($1 == "thickness")  print $2 }' input`
+#    NAME=$POINTNAME$THICKNESS
+#
+#    echo $NAME
+#
+#    create_smb
+#    convert_sim_sms $NAME.smd $NAME.sms $NAME.smb
+#fi
 
