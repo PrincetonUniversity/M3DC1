8d66f98b253825b37e8ece3b73f964802ead29fb
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2017-09-14 13:30:26 +0000

    * Removed hard-coded paths in sunfire.openmpi-1.10.3/part_mesh.sh

--- unstructured/sbin/sunfire.openmpi-1.10.3/part_mesh.sh
+++ unstructured/sbin/sunfire.openmpi-1.10.3/part_mesh.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-CONVERT=/p/tsc/m3dc1/lib/SCORECLib/rhel6/openmpi-1.10.3/utilities/create_mesh/convert_sim_sms
-SPLIT_SMB=/p/tsc/m3dc1/lib/SCORECLib/rhel6/openmpi-1.10.3/utilities/split_smb/split_smb
-MAKE_MODEL=/p/tsc/m3dc1/lib/SCORECLib/rhel6/openmpi-1.10.3/utilities/split_smb/make_model
 MESH_FILE=mesh.smb
 MODEL_FILE=model.dmg
 
@@ -13,14 +10,14 @@ else
     if [ ${1: -4} == ".smd" ]; then
 	echo "Splitting .sms file"
 	echo "*******************"
-	mpiexec -np 1 $CONVERT $1 $2 $MESH_FILE
-	mpiexec -np 1 $MAKE_MODEL $MESH_FILE $MODEL_FILE
-	mpiexec -np $3 $SPLIT_SMB $MODEL_FILE $MESH_FILE part.smb $3
+	mpiexec -np 1 convert_sim_sms $1 $2 $MESH_FILE
+	mpiexec -np 1 make_model $MESH_FILE $MODEL_FILE
+	mpiexec -np $3 split_smb $MODEL_FILE $MESH_FILE part.smb $3
     elif [ ${1: -4} == ".smb" ]; then
 	echo "Splitting .smb file"
 	echo "*******************"
-	mpiexec -np 1 $MAKE_MODEL $1 $MODEL_FILE
-	mpiexec -np $2 $SPLIT_SMB $MODEL_FILE $1 part.smb $2
+	mpiexec -np 1 make_model $1 $MODEL_FILE
+	mpiexec -np $2 split_smb $MODEL_FILE $1 part.smb $2
     fi
 fi
 
