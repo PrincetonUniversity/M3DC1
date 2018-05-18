b0d04a28162f3e307374055cafcd650793398363
Nate Ferraro <nathaniel.ferraro@gmail.com>
2018-05-18 11:23:22 -0400

    Revert "introduced temin_qd for equipartition term"
    This reverts commit ebd84e4aec0e715233928ffe9fbf09dd39e828a5.

--- unstructured/newpar.f90
+++ unstructured/newpar.f90
@@ -1333,9 +1333,8 @@ subroutine calculate_qdfac(itri, z)
         z = z + i**2 * tm79(:,OP_1) / kprad_mz
      end do
   end if
-  temp79a = max(temin_qd,real(tet79(:,OP_1)))
-  where(real(temp79a).gt.0.)
-     z = z * 3. * me_mp * nufac / temp79a**(3./2.)
+  where(real(tet79(:,OP_1)).gt.0.)
+     z = z * 3. * me_mp * nufac / tet79(:,OP_1)**(3./2.)
   elsewhere
      z = 0.
   end where
