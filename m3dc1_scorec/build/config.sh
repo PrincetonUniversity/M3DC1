27d5f6d839cd87b6db2d58f87dbe14790d3f54a0
William Tobin <tobinw2@rpi.edu>
2018-08-20 09:40:33 -0400

    simplifying the cmake build a bit, still more to do to add target exporting to ease possible downstream usage

--- m3dc1_scorec/build/config.sh
+++ m3dc1_scorec/build/config.sh
@@ -3,8 +3,8 @@
 # load a petsc module to get PETSC_DIR and PETSC_ARCH in the environment
 # ENABLE_COMPLEX=ON for complex build
 
-PREFIX=$DEVROOT/install/m3dc1_upstream
-SCOREC_DIR=$DEVROOT/install/core
+PREFIX=/fasttmp/wtobin/dev/install/m3dc1
+SCOREC_DIR=/fasttmp/wtobin/dev/install/core
 
 CC=`which mpicc`
 CXX=`which mpicxx`
@@ -15,9 +15,9 @@ cmake .. \
   -DCMAKE_CXX_COMPILER=$CXX \
   -DCMAKE_Fortran_COMPILER=$FORT \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
-  -DCMAKE_C_FLAGS=" -g -DDEBUG" \
-  -DCMAKE_CXX_FLAGS=" -g -DDEBUG" \
-  -DCMAKE_Fortran_FLAGS="-fpic "\
+  -DCMAKE_C_FLAGS=" -g -DDEBUG -fPIC" \
+  -DCMAKE_CXX_FLAGS=" -g -DDEBUG -fPIC" \
+  -DCMAKE_Fortran_FLAGS="-fPIC "\
   -DSCOREC_INCLUDE_DIR=$SCOREC_DIR/include \
   -DSCOREC_LIB_DIR=$SCOREC_DIR/lib \
   -DPETSC_DIR=$PETSC_DIR \
@@ -25,4 +25,5 @@ cmake .. \
   -DENABLE_COMPLEX=OFF \
   -DENABLE_TESTING=ON \
   -DCMAKE_BUILD_TYPE=Debug \
-  -DCMAKE_INSTALL_PREFIX="$PREFIX"
+  -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+  -DBUILD_SHARED_LIBS=FALSE
