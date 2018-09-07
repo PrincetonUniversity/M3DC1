23a19fcebc7b10d0b28186fc48f029dc84dcf19a
E. Seegyoung Seol <seols@rpi.edu>
2018-09-07 13:55:24 -0400

    config and makefile for eddy updated

--- unstructured/eddy.mk
+++ unstructured/eddy.mk
@@ -32,48 +32,37 @@ F90OPTS = $(F90FLAGS) $(FOPTS) -gen-interfaces
 F77OPTS = $(F77FLAGS) $(FOPTS)
 
 # define where you want to locate the mesh adapt libraries
-#HYBRID_HOME = /p/swim/jchen/hybrid.test
-#HYBRID_HOME = /u/iyamazak/release/v2/hybrid.test
-#HYBRID_LIBS = -L$(HYBRID_HOME)/lib -lhsolver
-
 PETSC_DIR=/home/jinchen/LIB/petsc-3.9.3
 ifeq ($(COM), 1)
 PETSC_ARCH=
 HYPRE_LIB=
 else
 PETSC_ARCH=eddy-intel-openmpi-real
-HYPRE_LIB=-lHYPRE
+HYPRE_LIB=
 endif
+BLASLAPACK_DIR=$(PETSC_DIR)/$(PETSC_ARCH)
+BLASLAPACK_LIBS =-Wl,-rpath,$(BLASLAPACK_DIR)/lib -L$(BLASLAPACK_DIR)/lib -lflapack -lfblas
 
-BLASLAPACK_LIBS =-Wl,-rpath,$(MKLROOT) -L$(MKLROOT)/lib/intel64 \
-        -lmkl_intel_lp64 -lmkl_sequential -lmkl_core
-HDF5_DIR=/usr/pppl/intel/2015-pkgs/openmpi-1.10.3-pkgs/hdf5-parallel-1.8.17
-SCOREC_DIR=/p/tsc/m3dc1/lib/SCORECLib/rhel6/Aug2017/openmpi-1.10.3/debug
-PUMI_LIB = -lpumi -lapf -lapf_zoltan -lcrv -lsam -lspr -lmth -lgmi -lma -lmds -lparma -lpcu -lph -llion
+SCALAPACK_DIR=$(PETSC_DIR)/$(PETSC_ARCH)
+SCALAPACK_LIB=-Wl,-rpath,$(SCALAPACK_DIR)/lib -L$(SCALAPACK_DIR) -lscalapack
 
-SCOREC_UTIL_DIR=/p/tsc/m3dc1/lib/SCORECLib/rhel6/openmpi-1.10.3/bin
+SCOREC_DIR=/home/jinchen/LIB/scorec/intel16.0-openmpi1.10.2/Sep2018
+SCOREC_UTIL_DIR=/home/jinchen/LIB/scorec/intel16.0-openmpi1.10.2/bin
+PUMI_LIBS = -lpumi -lapf -lapf_zoltan -lcrv -lsam -lspr -lmth -lgmi -lma -lmds -lparma -lpcu -lph -llion
 
-ifeq ($(TRILINOS), 1)
-  TRILINOS_DIR=/usr/pppl/intel/2015-pkgs/openmpi-1.10.3-pkgs/trilinos-11.12.1
-  ZOLTAN_LIB=-L$(TRILINOS_DIR)/lib -lzoltan
-  TRILINOS_LIBS = -Wl,--start-group,-rpath,$(TRILINOS_DIR)/lib -L$(TRILINOS_DIR)/lib \
-        -lstdc++  -lamesos -ltpetra -lkokkosnodeapi -ltpi -laztecoo -lepetra -lepetraext \
-        -lsacado -lteuchosparameterlist -lteuchoscomm -lteuchoscore -lteuchosnumerics -lteuchosremainder
-  PETSC_LIBS=
-else
-  ZOLTAN_LIB=-L/p/tsc/m3dc1/lib/SCORECLib/rhel6/openmpi-1.10.3/lib -lzoltan
-  TRILINOS_LIBS=
-  SCALAPACK_LIB=-Wl,-rpath,$(SCALAPACK_HOME)/lib -L$(SCALAPACK_HOME) -lscalapack
-  PETSC_LIBS = -L$(PETSC_DIR)/$(PETSC_ARCH)/lib -Wl,--start-group \
-        -lpetsc \
-        -lsuperlu_dist_3.3 -lsuperlu_4.3 \
-        -lcmumps -ldmumps -lsmumps -lzmumps -lmumps_common -lpord \
-        $(SCALAPACK_LIB) \
-        -lfftw3 -lfftw3_mpi \
-        $(HYPRE_LIB) \
-        -lparmetis -lmetis \
-        -Wl,--end-group
-endif
+ZOLTAN_LIB=-L$(PETSC_DIR)/$(PETSC_ARCH)/lib -lzoltan
+TRILINOS_LIBS=
+
+PETSC_LIBS = -L$(PETSC_DIR)/$(PETSC_ARCH)/lib -Wl,--start-group \
+      -lpetsc \
+      -lptscotcherr -lscotch -lptscotcherrexit -lscotcherr -lptesmumps -lscotcherrexit -lptscotch \
+      -lsuperlu_dist -lsuperlu \
+      -lcmumps -ldmumps -lsmumps -lesmumps -lzmumps -lmumps_common -lpord \
+      $(SCALAPACK_LIB) \
+      -lfftw3 -lfftw3_mpi \
+      $(HYPRE_LIB) \
+      -lparmetis -lmetis \
+      -Wl,--end-group
 
 ifeq ($(COM), 1)
   M3DC1_SCOREC_LIB=-lm3dc1_scorec_complex
@@ -85,23 +74,23 @@ else
   endif
 endif
 
-SCORECLIB= -Wl,--start-group,-rpath,$(SCOREC_DIR)/lib -L$(SCOREC_DIR)/lib \
-           $(PUMI_LIB) $(M3DC1_SCOREC_LIB) -Wl,--end-group
+SCOREC_LIBS= -Wl,--start-group,-rpath,$(SCOREC_DIR)/lib -L$(SCOREC_DIR)/lib \
+           $(PUMI_LIBS) $(M3DC1_SCOREC_LIB) -Wl,--end-group
 
 LIBS = 	\
-	$(SCORECLIB) \
+	$(SCOREC_LIBS) \
         $(TRILINOS_LIBS) \
         $(ZOLTAN_LIB) \
         $(PETSC_LIBS) \
         $(BLASLAPACK_LIBS) \
-	-L$(HDF5_DIR)/lib -lhdf5_fortran -lhdf5 -lz \
-	-L$(GSL_HOME)/lib -lgsl -lgslcblas \
+	-L$(HDF5DIR)/lib64 -lhdf5_fortran -lhdf5 -lz \
+	-L$(GSL_ROOT_DIR)/lib64 -lgsl -lgslcblas \
 	-lX11
 
 INCLUDE = -I$(PETSC_DIR)/include \
         -I$(PETSC_DIR)/$(PETSC_ARCH)/include \
-        -I$(HDF5_DIR)/include \
-        -I$(GSL_HOME)/include
+        -I$(HDF5DIR)/include \
+        -I$(GSL_ROOT_DIR)/include
 
 %.o : %.c
 	$(CC)  $(CCOPTS) $(INCLUDE) $< -o $@
