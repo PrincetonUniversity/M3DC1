a25ec36fec6f48bfd8ebda17c06de0bf71647eff
William Tobin <tobinw3@rpi.edu>
2018-11-27 16:53:18 -0500

    small modifications to the cmake configuration

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -2,7 +2,7 @@ find_library(FFTW_LIB fftw3)
 find_library(FFTW_THREADS_LIB fftw3_threads)
 
 set(FFTW_LIBRARIES ${FFTW_LIB} ${FFTW_THREADS_LIB})
-message(STATUS "FFTW : ${FFTW_LIBRARIES}")
+#message(STATUS "FFTW Libraries : ${FFTW_LIBRARIES}")
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FFTW DEFAULT_MSG FFTW_LIBRARIES)
 mark_as_advanced(FFTW_LIBRARIES)
--- cmake/FindGSL.cmake
+++ cmake/FindGSL.cmake
@@ -2,6 +2,7 @@ find_library(GSL_LIB gsl)
 find_library(GSL_CBLAS_LIB gslcblas)
 
 set(GSL_LIBRARIES ${GSL_LIB} ${GSL_CBLAS_LIB})
+#message(STATUS "GSL Libraries : ${GSL_LIBRARIES}")
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GSL DEFAULT_MSG GSL_LIBRARIES)
 mark_as_advanced(GSL_LIBRARIES)
