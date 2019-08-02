e98fce8c47a854dbe5f6f6c6880968912f60016b
seegyoung <seols@rpi.edu>
2019-08-01 17:13:33 -0700

    installing multiple SCOREC libraries on Cori

--- m3dc1_scorec/config-files/cori-mpich7.7.3-hsw-real-config.sh
+++ m3dc1_scorec/config-files/cori-mpich7.7.3-hsw-real-config.sh
@@ -4,12 +4,10 @@ ARCH=hsw
 CMAKETYPE=Release
 PETSC_VER=petsc-3.9.3
 PETSCVER=petsc3.9.3
-#PETSC_DIR=/global/project/projectdirs/mp288/jinchen/PETSC/$PETSC_VER
-#PETSC_ARCH=cori-hsw-mpich773-real-nomkl-510
-PETSC_DIR=/global/homes/j/jinchen/project/PETSC/$PETSC_VER
-PETSC_ARCH=cori-hsw-mpich776-cplx-nomkl-510
-#/global/project/projectdirs/mp288/cori/petsc/$PETSC_VER
-#PETSC_ARCH=real-intel-mpi7.7.3-$ARCH
+PETSC_DIR=/global/project/projectdirs/mp288/cori/petsc/$PETSC_VER
+PETSC_ARCH=real-intel-$MPIVER-$ARCH
+#PETSC_DIR=/global/homes/j/jinchen/project/PETSC/$PETSC_VER
+#PETSC_ARCH=cori-hsw-mpich776-real-nomkl-510
 #load module cray-hdf5-parallel
 PARMETIS_DIR=$PETSC_DIR/$PETSC_ARCH
 ZOLTAN_DIR=/global/project/projectdirs/mp288/cori/scorec/$MPIVER/$ARCH-$PETSCVER
@@ -32,5 +30,5 @@ cmake .. \
   -DHDF5_LIB_DIR="$HDF5_DIR/lib" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DENABLE_TESTING=OFF \
-  -DENABLE_COMPLEX=ON \
+  -DENABLE_COMPLEX=OFF \
   -DCMAKE_BUILD_TYPE=$CMAKETYPE
