25829627b63112aaf8c2f949de57d2c553ddeaa7
Seegyoung Seol <sseol@traverse.princeton.edu>
2019-11-06 11:56:15 -0500

    adding traverse config file

---
+++ m3dc1_scorec/config-files/traverse-real-config.sh
@@ -0,0 +1,32 @@
+MPIVER=pgi19.9-openmpi4.0.2
+CMAKETYPE=Release
+PETSC_VER=petsc-3.12.0
+PETSCVER=petsc3.12.0
+PETSC_DIR=/home/jinchen/project/PETSC/$PETSC_VER
+PETSC_ARCH=traverse-pgi-openmpi-2019
+#load module load pgi openmpi/pgi-19.5/4.0.2rc1/64
+PARMETIS_DIR=$PETSC_DIR/$PETSC_ARCH
+SCOREC_DIR=/projects/M3DC1/scorec/$MPIVER/$PETSCVER
+PREFIX=$SCOREC_DIR
+#add -DPETSCMASTER for petsc 3.8.3 or higher
+cmake3 .. \
+  -DCMAKE_C_COMPILER=mpicc \
+  -DCMAKE_CXX_COMPILER=mpic++ \
+  -DCMAKE_Fortran_COMPILER=mpif90 \
+  -DCMAKE_C_FLAGS=" -g -O0 -DPETSCMASTER -I$PETSC_DIR/include" \
+  -DCMAKE_CXX_FLAGS=" -g -O0 -DPETSCMASTER -I$PETSC_DIR/include" \
+  -DCMAKE_Fortran_FLAGS="-fpic "\
+  -DSCOREC_INCLUDE_DIR="$SCOREC_DIR/include" \
+  -DSCOREC_LIB_DIR="$SCOREC_DIR/lib" \
+  -DZOLTAN_LIBRARY="$ZOLTAN_DIR/lib/libzoltan.a" \
+  -DPARMETIS_LIBRARY="$PARMETIS_DIR/lib/libparmetis.a" \
+  -DMETIS_LIBRARY="$PARMETIS_DIR/lib/libmetis.a" \
+  -DPETSC_INCLUDE_DIR="$PETSC_DIR/$PETSC_ARCH/include" \
+  -DPETSC_LIB_DIR="$PETSC_DIR/$PETSC_ARCH/lib" \
+  -DHDF5_INCLUDE_DIR="$HDF5_DIR/include" \
+  -DHDF5_LIB_DIR="$HDF5_DIR/lib64" \
+  -DENABLE_PETSC=ON \
+  -DENABLE_COMPLEX=OFF \
+  -DENABLE_TESTING=OFF \
+  -DCMAKE_BUILD_TYPE=$CMAKETYPE \
+  -DCMAKE_INSTALL_PREFIX="$PREFIX"
