0c8c90c63b516d183d3903933d8687a92701499f
Brendan C. Lyons <lyonsbc@fusion.gat.com>
2020-06-08 14:00:30 -0400

    Read psibound as psi_lcfs at restart

--- unstructured/restart_hdf5.f90
+++ unstructured/restart_hdf5.f90
@@ -116,7 +116,7 @@ contains
     call read_scalar(scalar_group_id, "znull2"      , znull2t  , ntime, error)
     call read_scalar(scalar_group_id, "xmag"        , xmag     , ntime, error)
     call read_scalar(scalar_group_id, "zmag"        , zmag     , ntime, error)
-    call read_scalar(scalar_group_id, "psibound"    , psibound , ntime, error)
+    call read_scalar(scalar_group_id, "psi_lcfs"    , psibound , ntime, error)
     call read_scalar(scalar_group_id, "psimin"      , psimin   , ntime, error)
 
     if(mod_null_rs .eq.0) then
