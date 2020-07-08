3d6c753954d605d5df03f718254bdb7b152a81c1
Nate Ferraro <nathaniel.ferraro@gmail.com>
2020-07-08 09:47:07 -0400

    Moved auto-run.sh and auto-check.sh to unstructured/regtest (for now). Added separate platform-specific configuration file

--- unstructured/regtest/auto-run.sh
+++ unstructured/regtest/auto-run.sh
@@ -1,22 +1,20 @@
 #!/bin/csh
 
+if ( $#argv == 0 ) then
+    echo "Usage: auto-run.sh config-[arch].sh"
+    exit(1)
+endif
+
 echo "================================================="
 date
 echo " _______________________"
 hostname
 
-setenv M3DC1_CODE_DIR "/p/tsc/C1/src"
+source $argv[1]
+
 cd $M3DC1_CODE_DIR
 rm unstructured/regtest/run.log
 
-module purge
-module use /p/m3dc1/modules
-module load m3dc1/devel-centos7
-module load git
-module list
-printenv PATH
-
-setenv GIT_SSH_COMMAND 'ssh -i /u/nferraro/.ssh/id_rsa_0 -l nferraro' 
 git pull
 cd unstructured
 make OPT=1 clean; make OPT=1 COM=1 clean; make OPT=1 3D=1 MAX_PTS=60 clean
@@ -26,5 +24,5 @@ make OPT=1 3D=1 MAX_PTS=60
 make bin
 
 cd regtest
-./run centos7 > run.log
+./run $BATCH_SUFFIX > run.log
 cat run.log
