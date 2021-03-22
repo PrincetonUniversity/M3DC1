33fd2267b9b3635357b1e041b6015c1370985141
Seegyoung Seol <sseol@stellar-intel.princeton.edu>
2021-03-22 14:35:56 -0400

    updating stellar.mk to use new SCOREC build

--- unstructured/stellar.mk
+++ unstructured/stellar.mk
@@ -1,5 +1,5 @@
-FOPTS = -c -r8 -implicitnone -fpp -warn all $(OPTS) -DUSEBLAS -DPETSC_VERSION=312
-CCOPTS  = -c -DPETSC_VERSION=312 -DSTELLAR
+FOPTS = -c -r8 -implicitnone -fpp -warn all $(OPTS) -DUSEBLAS -DPETSC_VERSION=313
+CCOPTS  = -c -DPETSC_VERSION=313
 
 ifeq ($(OPT), 1)
   FOPTS  := $(FOPTS) -O2 -qopt-report=0 -qopt-report-phase=vec
@@ -13,62 +13,46 @@ ifeq ($(PAR), 1)
   FOPTS := $(FOPTS) -DUSEPARTICLES
 endif
 
-ifeq ($(TAU), 1)
-  TAU_OPTIONS = -optCPPOpts=-DUSETAU -optVerbose -optPreProcess -optMpi -optTauSelectFile=../select.tau
-  CC     = tau_cc.sh $(TAU_OPTIONS)
-  CPP    = tau_cxx.sh $(TAU_OPTIONS)
-  F90    = tau_f90.sh $(TAU_OPTIONS)
-  F77    = tau_f90.sh $(TAU_OPTIONS)
-  LOADER = tau_f90.sh $(TAU_OPTIONS)
-else
-  CC = mpicc
-  CPP = mpicxx
-  F90 = mpif90
-  F77 = mpif90
-  LOADER = mpif90
-  LDOPTS := $(LDOPTS) -cxxlib
-endif
+CC = /usr/local/openmpi/4.1.0/intel20211/bin/mpicc
+CPP = /usr/local/openmpi/4.1.0/intel20211/bin/mpicxx
+F90 =/usr/local/openmpi/4.1.0/intel20211/bin/mpif90
+F77 = /usr/local/openmpi/4.1.0/intel20211/bin/mpif90
+LOADER =/usr/local/openmpi/4.1.0/intel20211/bin/mpif90
+LDOPTS := $(LDOPTS) -cxxlib
 F90OPTS = $(F90FLAGS) $(FOPTS) -gen-interfaces
 F77OPTS = $(F77FLAGS) $(FOPTS)
 
 # define where you want to locate the mesh adapt libraries
-PETSCVER=petsc3.12.0
-PETSC_VER=petsc-3.12.0
-PETSC_DIR=/projects/M3DC1/PETSC/petsc.stellar
+MPIVER=intel2021.1.2-openmpi4.1.0
+PETSC_VER=petsc-3.13.5
+PETSCVER=petsc3.13.5
+PETSC_DIR=/projects/M3DC1/PETSC/$(PETSC_VER)
 ifeq ($(COM), 1)
-PETSC_ARCH=cplx-stella-openmpi-2021
+  PETSC_ARCH=cplx-$(MPIVER)
+  M3DC1_SCOREC_LIB=-lm3dc1_scorec_complex 
 else
-PETSC_ARCH=real-stella-openmpi-2021
-endif
-
-MPI_DIR=/opt/intel/compilers_and_libraries_2018.3.222/linux/mpi
-INTEL_DIR=/opt/intel/compilers_and_libraries_2018.3.222/linux
-GCC_DIR=/usr/lib/gcc/x86_64-redhat-linux/4.8.5
-
-PETSC_WITH_EXTERNAL_LIB = -L${PETSC_DIR}/${PETSC_ARCH}/lib -Wl,-rpath,${PETSC_DIR}/${PETSC_ARCH}/lib -L${PETSC_DIR}/${PETSC_ARCH} -L/usr/local/openmpi/4.1.0/intel20211/lib64 -L/opt/intel/oneapi/mkl/2021.1.1/lib/intel64 -L/opt/intel/oneapi/tbb/2021.1.1/lib/intel64/gcc4.8 -L/opt/intel/oneapi/compiler/2021.1.2/linux/lib -L/opt/intel/oneapi/compiler/2021.1.2/linux/compiler/lib/intel64_lin -L/usr/lib/gcc/x86_64-redhat-linux/8 -Wl,-rpath,/usr/local/openmpi/4.1.0/intel20211/lib64 -lpetsc -lcmumps -ldmumps -lsmumps -lzmumps -lmumps_common -lpord -lscalapack -lsuperlu -lsuperlu_dist -lflapack -lfblas -lzoltan -lparmetis -lmetis -lz -lstdc++ -ldl  -lmpi_cxx -lmpi_usempif08 -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi -lifport -lifcoremt -limf -lsvml -lm -lipgo -lirc -lpthread -lgcc_s -lirc_s -lquadmath -lstdc++ -ldl
-
-ifeq ($(COM), 1)
-  M3DC1_SCOREC_LIB=-lm3dc1_scorec_complex
-else
+  PETSC_ARCH=real-$(MPIVER)
   M3DC1_SCOREC_LIB=-lm3dc1_scorec
 endif
 
-SCOREC_BASE_DIR=${PETSC_DIR}/${PETSC_ARCH}
+PETSC_WITH_EXTERNAL_LIB = -L${PETSC_DIR}/${PETSC_ARCH}/lib -Wl,-rpath,${PETSC_DIR}/${PETSC_ARCH}/lib -L${PETSC_DIR}/${PETSC_ARCH}/lib -Wl,-rpath,/usr/local/fftw/intel-2021.1/openmpi-4.1.0/3.3.9/lib -L/usr/local/fftw/intel-2021.1/openmpi-4.1.0/3.3.9/lib -Wl,-rpath,/usr/local/hdf5/intel-2021.1/openmpi-4.1.0/1.10.6/lib64 -L/usr/local/hdf5/intel-2021.1/openmpi-4.1.0/1.10.6/lib64 -L/usr/local/openmpi/4.1.0/intel20211/lib64 -L/usr/local/fftw/intel-2021.1/openmpi-4.1.0/3.3.9/lib64 -L/opt/intel/oneapi/mkl/2021.1.1/lib/intel64 -L/opt/intel/oneapi/tbb/2021.1.1/lib/intel64/gcc4.8 -L/opt/intel/oneapi/compiler/2021.1.2/linux/lib -L/opt/intel/oneapi/compiler/2021.1.2/linux/compiler/lib/intel64_lin -L/usr/lib/gcc/x86_64-redhat-linux/8 -Wl,-rpath,/usr/local/openmpi/4.1.0/intel20211/lib64 -lpetsc -lcmumps -ldmumps -lsmumps -lzmumps -lmumps_common -lpord -lscalapack -lsuperlu -lsuperlu_dist -lfftw3_mpi -lfftw3 -lflapack -lfblas -lzoltan -lX11 -lhdf5hl_fortran -lhdf5_fortran -lhdf5_hl -lhdf5 -lparmetis -lmetis -lz -lstdc++ -ldl -lmpi_usempif08 -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi -lifport -lifcoremt_pic -limf -lsvml -lm -lipgo -lirc -lpthread -lgcc_s -lirc_s -lquadmath -lstdc++ -ldl
+
+SCOREC_BASE_DIR=/projects/M3DC1/scorec/$(MPIVER)/$(PETSCVER)
 SCOREC_UTIL_DIR=$(SCOREC_BASE_DIR)/bin
 ifdef SCORECVER
-  SCOREC_DIR=$(SCOREC_BASE_DIR)
+  SCOREC_DIR=$(SCOREC_BASE_DIR)/$(SCORECVER)
 else
   SCOREC_DIR=$(SCOREC_BASE_DIR)
 endif
 
-ZOLTAN_LIB=-L$(SCOREC_BASE_DIR)/lib -lzoltan
+ZOLTAN_LIB=-L$(PETSC_DIR)/$(PETSC_ARCH)/lib -lzoltan
 
 SCOREC_LIBS= -L$(SCOREC_DIR)/lib $(M3DC1_SCOREC_LIB) \
              -Wl,--start-group,-rpath,$(SCOREC_BASE_DIR)/lib -L$(SCOREC_BASE_DIR)/lib \
              -lpumi -lapf -lapf_zoltan -lgmi -llion -lma -lmds -lmth -lparma \
              -lpcu -lph -lsam -lspr -lcrv -Wl,--end-group
 
-LIBS = 	\
+LIBS = 	-L$(OPENMPI_HOME)/lib64 -lmpi_cxx\
 	$(SCOREC_LIBS) \
         $(ZOLTAN_LIB) \
         $(PETSC_WITH_EXTERNAL_LIB) \
