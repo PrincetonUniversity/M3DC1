0667ec21dad2833a46eb5f485417248de5263075
Morteza H. Siboni <hakimm2@rpi.edu>
2021-09-18 10:52:26 -0400

    Adds the new apis to name_convert.h
    To make them accessible on the fortran side

--- m3dc1_scorec/api/name_convert.h
+++ m3dc1_scorec/api/name_convert.h
@@ -17,6 +17,7 @@
 #define m3dc1_mesh_load m3dc1_mesh_load_
 #define m3dc1_mesh_load_3d m3dc1_mesh_load_3d_
 #define m3dc1_mesh_build3d m3dc1_mesh_build3d_
+#define m3dc1_spr_then_adapt m3dc1_spr_then_adapt_
 #define m3dc1_mesh_adapt m3dc1_mesh_adapt_
 #define m3dc1_mesh_write m3dc1_mesh_write_
 #define m3dc1_mesh_getnument m3dc1_mesh_getnument_
@@ -59,6 +60,7 @@
 #define m3dc1_region_getoriginalface m3dc1_region_getoriginalface_
 #define m3dc1_field_getnewid m3dc1_field_genid_
 #define m3dc1_field_create m3dc1_field_create_
+#define m3dc1_mark_for_solutiontransfer m3dc1_mark_for_solutiontransfer_
 #define m3dc1_field_delete m3dc1_field_delete_
 #define m3dc1_field_exist m3dc1_field_exist_
 #define m3dc1_field_sync m3dc1_field_sync_
