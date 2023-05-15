6cf8485c8106e1c35fe56fbb062131c76b535267
Min-Gu Yoo <mgyoo86@gmail.com>
2023-05-15 10:53:45 -0700

    Update newsolve output message

--- unstructured/scorec_matrix.f90
+++ unstructured/scorec_matrix.f90
@@ -411,7 +411,7 @@ contains
 #endif
     ierr = 0
     t2=MPI_WTIME()
-    if(myrank==0) write(*,'(A,I0,A,ES10.2,A)') "For mat%imatrix=",mat%imatrix,", newsolve takes ", t2-t1, " secs"
+
 #ifdef M3DC1_TRILINOS
     call m3dc1_solver_getnumiter(mat%imatrix,num_iter)
 #else
@@ -419,6 +419,7 @@ contains
     call m3dc1_matrix_getiternum(mat%imatrix,num_iter)
 !    if(mat%imatrix==5) call m3dc1_matrix_getinfo(mat%imatrix,v%id)
 #endif
+   if(myrank==0) write(*,'(A,I4,A,I5,A,ES11.3,A)') "For mat%imatrix=",mat%imatrix,", newsolve takes num_iter=",num_iter,", time=", t2-t1, " secs"
    if (num_iter.ge.10000) then
       print*, 'Error: solver', mat%imatrix, 'diverged. JOB stopped'
       call safestop(10000)
@@ -461,13 +462,14 @@ contains
 #endif
    ierr = 0
    t2=MPI_WTIME()
-   if(myrank==0) write(*,'(A,I0,A,ES10.2,A)') "For mat%imatrix=",mat%imatrix,", newsolve_with_guess takes ", t2-t1, " secs"
 
 #ifdef M3DC1_TRILINOS
    call m3dc1_solver_getnumiter(mat%imatrix,num_iter)
 #else
    call m3dc1_matrix_getiternum(mat%imatrix,num_iter)
 #endif
+
+  if(myrank==0) write(*,'(A,I4,A,I5,A,ES11.3,A)') "For mat%imatrix=",mat%imatrix,", newsolve_with_guess takes num_iter=",num_iter,", time=", t2-t1, " secs"
   if (num_iter.ge.10000) then
      print*, 'Error: solver', mat%imatrix, 'diverged. JOB stopped'
      call safestop(10000)
